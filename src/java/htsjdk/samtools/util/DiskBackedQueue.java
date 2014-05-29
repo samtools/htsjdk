02dd599fda279f5833bac4459a7e100861e675fb
bradtaylor <bradt@broadinstitute.org>
2014-05-29 17:52:26 -0400

    Changed DiskBackedQueue to implement Queue interface, not iterator
    Still lots of work to do. Just a commit before going home on 5/29

--- src/java/htsjdk/samtools/util/DiskBackedQueue.java
+++ src/java/htsjdk/samtools/util/DiskBackedQueue.java
@@ -23,60 +23,69 @@
  */
 package htsjdk.samtools.util;
 
+import com.sun.jndi.url.iiopname.iiopnameURLContextFactory;
 import net.sf.samtools.Defaults;
-import net.sf.samtools.SAMException;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Deque;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Queue;
 
 /**
- * An iterator intended for use as a queue. Writes elements to temporary files when the queue gets too big
+ * A FIFO queue. Writes elements to temporary files when the queue gets too big.
  * External references to elements in this queue are NOT guaranteed to be valid, due to the disk write/read
- *
+ * <p/>
+ * NB: The queue becomes read-only after the first on-disk record is read. Max size is therefore non-deterministic.
+ * This avoids issues arising from conflicts between the input and output streams.
+ * This could perhaps be avoided by creating a version of BAMRecordCodec that operates on RandomAccessFiles or channels.
+ * <p/>
  * Created by bradt on 4/28/14.
  */
-public class DiskBackedQueue<T> implements Iterator<T> {
+public class DiskBackedQueue<E> implements Queue<E> {
 
     // TODO - this needs to be altered (and simplified significantly) to conform with the storage behavior we expect for MarkDuplicatesWithMateCigar BufferBlock
 
-    private final int maxRecordsInRam;
-    private final Deque<T> ramRecords;
+    /** TODO - support peeking. Add a "nextRecord" member. Return this if we are peeking, and update it if polling or removing.
+     Move the record reading (current 'poll') to a private method  **/
 
-    /** Directories where files of records go. */
+    private final int maxRecordsInRam;
+    private final Deque<E> ramRecords;
+    private File diskRecords = null;
+    private final TempStreamFactory tempStreamFactory = new TempStreamFactory();
+    private OutputStream outputStream = null;
+    private InputStream inputStream = null;
+    private boolean canAdd = false;
+
+    /** Record representing the head of the queue; returned by peek, poll **/
+    private E headRecord;
+
+    /** Directories where file of records go. **/
     private final List<File> tmpDirs;
 
     /**
      * Used to write records to file, and used as a prototype to create codecs for reading.
      */
-    private final SortingCollection.Codec<T> codec;
+    private final SortingCollection.Codec<E> codec;
 
-    /**
-     *
-     * List of files in tmpDir containing sorted records, their associated output streams, and a map counting their contents
-     */
-    private final List<File> files = new ArrayList<File>();
-    private final List<OutputStream> outputStreams = new ArrayList<OutputStream>();
-    private final Map<File, Integer> elementsInFile = new HashMap<File, Integer>();
-    private final TempStreamFactory tempStreamFactory = new TempStreamFactory();
 
     /**
      * Prepare to accumulate records
+     *
      * @param codec For writing records to file and reading them back into RAM
      * @param maxRecordsInRam how many records to accumulate before spilling to disk
      * @param tmpDir Where to write files of records that will not fit in RAM
      */
-    private DiskBackedQueue(final SortingCollection.Codec<T> codec,
+    private DiskBackedQueue(final SortingCollection.Codec<E> codec,
                             final int maxRecordsInRam, final List<File> tmpDir) {
         if (maxRecordsInRam <= 0) {
             throw new IllegalArgumentException("maxRecordsInRam must be > 0");
@@ -87,7 +96,7 @@ public class DiskBackedQueue<T> implements Iterator<T> {
         this.tmpDirs = tmpDir;
         this.codec = codec;
         this.maxRecordsInRam = maxRecordsInRam;
-        this.ramRecords = new ArrayDeque<T>();
+        this.ramRecords = new ArrayDeque<E>();
     }
 
     /**
@@ -98,92 +107,119 @@ public class DiskBackedQueue<T> implements Iterator<T> {
      * @param tmpDir Where to write files of records that will not fit in RAM
      */
     public static <T> DiskBackedQueue<T> newInstance(final SortingCollection.Codec<T> codec,
-                                                       final int maxRecordsInRAM,
-                                                       final List<File> tmpDir) {
+                                                     final int maxRecordsInRAM,
+                                                     final List<File> tmpDir) {
         return new DiskBackedQueue<T>(codec, maxRecordsInRAM, tmpDir);
 
     }
 
+    public boolean canAdd() {
+        return canAdd;
+    }
+
     /**
-     * Add the record to the queue, spilling to disk if necessary
+     * Add the record to the tail of the queue, spilling to disk if necessary
+     * Must check that (canAdd() == true) before calling this method
      */
-    public void add(final T record) {
-        if (files.size() != 0 || ramRecords.size() == maxRecordsInRam) {
-            spillToDisk(record);
-        }
-        else {
-            ramRecords.add(record);
+    public boolean add(final E record) throws IllegalStateException {
+        if (canAdd) {
+            if (diskRecords != null || ramRecords.size() == maxRecordsInRam)
+                spillToDisk(record);
+            else
+                ramRecords.add(record);
+            return true;
+        } else {
+            throw new IllegalStateException("cannot add to DiskBackedQueue whose canAdd() method returns false");
         }
     }
 
     @Override
-    public boolean hasNext() {
-        return !(ramRecords.isEmpty() && files.size() == 0);
+    public boolean offer(final E e) {
+        return this.canAdd && this.add(e);
     }
 
-    /**
-     * Return the first element at the head of the queue, but do not remove it
-     */
-    public T peek() {
+    @Override
+    public E remove() {
+        final E element = this.poll();
+        if (element == null)
+            throw new NoSuchElementException("Attempting to remove() from empty DiskBackedQueue");
+        else
+            return element;
+    }
+
+    @Override
+    public E poll() {
+        return (ramRecords.isEmpty()) ? ramRecords.peekFirst() : readFileRecord(this.diskRecords);
+    }
+
+    @Override
+    public E element() {
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        // TODO - fix
         if (!ramRecords.isEmpty()) {
             return ramRecords.peekFirst();
-        }
-        else if (files.size() != 0) {
+        } else if (files.size() != 0) {
             final File currentFile = files.remove(0);  // get the file nearest the head of the queue
             CloserUtil.close(outputStreams.remove(0)); // close the file's associated output stream
-            readFileIntoRam(currentFile);
+            readFileRecord(currentFile);
             return ramRecords.peekFirst();
         }
         return null;
     }
 
     @Override
-    public T next() {
-        if (!ramRecords.isEmpty()) {
-            return ramRecords.pollFirst();
-        }
-        else if (files.size() != 0) {
-            final File currentFile = files.remove(0);  // get the file nearest the head of the queue
-            CloserUtil.close(outputStreams.remove(0)); // close the file's associated output stream
-            readFileIntoRam(currentFile);
-            return ramRecords.pollFirst();
-        }
-        return null;
+    public boolean containsAll(final Collection<?> c) {
+        return false;
     }
+
+    @Override
+    public boolean addAll(final Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(final Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(final Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(final Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
 
     /**
      * Write the present record to the end of a file representing the tail of the queue.
-     * The tail of the queue should always be the last file in the files list
      */
-    private void spillToDisk(final T record) {
+    private void spillToDisk(final E record) throws RuntimeIOException {
         try {
-            OutputStream os;
-            File f;
-            if (this.files.size() == 0 || elementsInFile.get(this.files.get(this.files.size() - 1)) == maxRecordsInRam ) {
-                f = newTempFile();
-                os = tempStreamFactory.wrapTempOutputStream(new FileOutputStream(f), Defaults.BUFFER_SIZE);
-                this.files.add(f);
-                this.outputStreams.add(os);
-            }
-            else {
-                f = this.files.get(this.files.size() - 1);
-                os = this.outputStreams.get(this.outputStreams.size() - 1);
+            if (this.diskRecords == null) {
+                this.diskRecords = newTempFile();
+                this.outputStream = tempStreamFactory.wrapTempOutputStream(new FileOutputStream(this.diskRecords), Defaults.BUFFER_SIZE);
             }
             try {
-                this.codec.setOutputStream(os);
+                this.codec.setOutputStream(this.outputStream);
                 this.codec.encode(record);
-                os.flush();
-            }
-            catch (final RuntimeIOException ex) {
+                this.outputStream.flush();
+            } catch (final RuntimeIOException ex) {
                 throw new RuntimeIOException("Problem writing temporary file. " +
-                                             "Try setting TMP_DIR to a file system with lots of space.", ex);
+                        "Try setting TMP_DIR to a file system with lots of space.", ex);
             }
-            Integer elements = this.elementsInFile.get(f);
-            if (elements == null)
-                elements = 0;
-            this.elementsInFile.put(f, ++elements);
-        }
-        catch (final IOException e) {
+        } catch (final IOException e) {
             throw new RuntimeIOException(e);
         }
     }
@@ -198,34 +234,23 @@ public class DiskBackedQueue<T> implements Iterator<T> {
 
 
     /**
-     * Read one of our disk-written files back into RAM
+     * Read back a record that had been spilled to disk.
+     * Note- if we are reading disk records, we can no longer add additional elements to this DiskBackedQueue
+     *
+     * @param file the file to read from
+     * @return The next element from the head of the file, or null if end-of-file is reached
      */
-    private void readFileIntoRam(final File file) {
-        InputStream inputStream = null;
+    private E readFileRecord (final File file) {
+        if (this.canAdd)
+            this.canAdd = false; // NB: should this just be an assignment regardless?
         try {
-            inputStream = new FileInputStream(file);
-            this.codec.setInputStream(tempStreamFactory.wrapTempInputStream(inputStream, Defaults.BUFFER_SIZE));
-            T record;
-            while ((record = this.codec.decode()) != null) {
-                ramRecords.addLast(record);
+            if (this.inputStream == null) {
+                inputStream = new FileInputStream(file);
+                this.codec.setInputStream(tempStreamFactory.wrapTempInputStream(inputStream, Defaults.BUFFER_SIZE));
             }
-            IOUtil.deleteFiles(file);
-        }
-        catch (final IOException ex) {
-            throw new RuntimeIOException("DiskBackedQueue tried to read from a file that did not exist");
-        }
-        finally {
-            CloserUtil.close(inputStream);
-        }
-    }
-
-    /**
-     * Pop the first element from the head of the queue, but do not return it
-     */
-    @Override
-    public void remove() {
-        if (this.next() == null) {
-            throw new SAMException("trying to remove from empty DiskBackedQueue");
+            return this.codec.decode(); // NB: returns null if end-of-file is reached.
+        } catch (final IOException e) {
+            throw new RuntimeIOException("DiskBackedQueue encountered an error reading from a file", e);
         }
     }
 
@@ -234,5 +259,30 @@ public class DiskBackedQueue<T> implements Iterator<T> {
      */
     public int size() {
         return (files.size() * maxRecordsInRam) + ramRecords.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(final Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T1> T1[] toArray(final T1[] a) {
+        return null;
     }
 }
