665ea64b38aad8c20d781cd95f911f6abe9a1507
dmyersturnbull <dmyersturnbull@gmail.com>
2014-09-05 17:56:00 -0700

    Replaced use of TestNG in SAMTestUtil.

--- src/java/htsjdk/samtools/SAMTestUtil.java
+++ src/java/htsjdk/samtools/SAMTestUtil.java
@@ -23,68 +23,106 @@
  */
 package htsjdk.samtools;
 
-import org.testng.Assert;
-
 /**
  * Misc methods for SAM-related unit tests.  These are in the src tree rather than the tests tree
  * so that they will be included in sam.jar, and therefore can be used by tests outside of htsjdk.samtools.
- * These methods use org.testng.Assert methods.
  */
 public class SAMTestUtil {
+
+    /**
+     * Indicates that a required sanity-check condition was not met.
+     */
+    public static class SanityCheckFailedException extends RuntimeException {
+        public SanityCheckFailedException(String message) {
+            super(message);
+        }
+    }
+
     /**
      * Basic sanity check for a pair of SAMRecords.
+     * @throws SanityCheckFailedException if the sanity check failed
      */
-    public void assertPairValid(final SAMRecord firstEnd, final SAMRecord secondEnd) {
-        Assert.assertEquals(firstEnd.getReadName(), secondEnd.getReadName());
-        Assert.assertTrue(firstEnd.getFirstOfPairFlag());
-        Assert.assertTrue(secondEnd.getSecondOfPairFlag());
-        Assert.assertFalse(secondEnd.getFirstOfPairFlag());
-        Assert.assertFalse(firstEnd.getSecondOfPairFlag());
+    public void assertPairValid(final SAMRecord firstEnd, final SAMRecord secondEnd) throws SanityCheckFailedException {
+        assertEquals(firstEnd.getReadName(), secondEnd.getReadName());
+        assertTrue(firstEnd.getFirstOfPairFlag());
+        assertTrue(secondEnd.getSecondOfPairFlag());
+        assertFalse(secondEnd.getFirstOfPairFlag());
+        assertFalse(firstEnd.getSecondOfPairFlag());
         if (!firstEnd.getReadUnmappedFlag() && !secondEnd.getReadUnmappedFlag()) {
-            Assert.assertNotSame(firstEnd.getReadNegativeStrandFlag(),
+            assertNotSame(firstEnd.getReadNegativeStrandFlag(),
                     secondEnd.getReadNegativeStrandFlag());
         }
     }
 
     /**
      * Basic sanity check for a SAMRecord.
+     * @throws SanityCheckFailedException if the sanity check failed
      */
-    public void assertReadValid(final SAMRecord read) {
-        Assert.assertEquals(read.getReadBases().length, read.getBaseQualities().length);
+    public void assertReadValid(final SAMRecord read) throws SanityCheckFailedException {
+        assertEquals(read.getReadBases().length, read.getBaseQualities().length);
         // Note that it is possible to have an unmapped read that has a coordinate
         if (read.getReferenceName().equals(SAMRecord.NO_ALIGNMENT_REFERENCE_NAME)) {
-            Assert.assertEquals(read.getAlignmentStart(), SAMRecord.NO_ALIGNMENT_START);
-            Assert.assertTrue(read.getReadUnmappedFlag());
+            assertEquals(read.getAlignmentStart(), SAMRecord.NO_ALIGNMENT_START);
+            assertTrue(read.getReadUnmappedFlag());
         } else {
-            Assert.assertNotSame(read.getAlignmentStart(), SAMRecord.NO_ALIGNMENT_START);
+            assertNotSame(read.getAlignmentStart(), SAMRecord.NO_ALIGNMENT_START);
         }
         if (read.getReadUnmappedFlag()) {
-            Assert.assertEquals(read.getMappingQuality(), SAMRecord.NO_MAPPING_QUALITY);
-            Assert.assertEquals(read.getCigar().getCigarElements().size(), 0);
+            assertEquals(read.getMappingQuality(), SAMRecord.NO_MAPPING_QUALITY);
+            assertEquals(read.getCigar().getCigarElements().size(), 0);
         } else {
-            Assert.assertNotSame(read.getCigar().getCigarElements(), 0);
+            assertNotSame(read.getCigar().getCigarElements(), 0);
         }
         if (read.getReadPairedFlag()) {
             if (read.getMateReferenceName().equals(SAMRecord.NO_ALIGNMENT_REFERENCE_NAME)) {
-                Assert.assertEquals(read.getMateAlignmentStart(), SAMRecord.NO_ALIGNMENT_START);
-                Assert.assertTrue(read.getMateUnmappedFlag());
+                assertEquals(read.getMateAlignmentStart(), SAMRecord.NO_ALIGNMENT_START);
+                assertTrue(read.getMateUnmappedFlag());
             } else {
                 // Even if the mate is unmapped, if it has a reference name, it should have a position.
-                Assert.assertNotSame(read.getMateAlignmentStart(), SAMRecord.NO_ALIGNMENT_START);
+                assertNotSame(read.getMateAlignmentStart(), SAMRecord.NO_ALIGNMENT_START);
             }
             if (read.getReadUnmappedFlag() || read.getMateUnmappedFlag() ||
                     !read.getReferenceName().equals(read.getMateReferenceName())) {
-                Assert.assertEquals(read.getInferredInsertSize(), 0);
+                assertEquals(read.getInferredInsertSize(), 0);
             } else {
-                Assert.assertNotSame(read.getInferredInsertSize(), 0);
+                assertNotSame(read.getInferredInsertSize(), 0);
             }
             if (!read.getReadUnmappedFlag() && !read.getMateUnmappedFlag()) {
-                Assert.assertNotSame(read.getReadNegativeStrandFlag(), read.getMateNegativeStrandFlag(),
+                assertNotSame(read.getReadNegativeStrandFlag(), read.getMateNegativeStrandFlag());
+                assertNotSame(read.getMateNegativeStrandFlag(),
                         read.getReadName());
             }
 
         } else {
-            Assert.assertEquals(read.getInferredInsertSize(), 0);
+            assertEquals(read.getInferredInsertSize(), 0);
+        }
+    }
+
+    private static <T> void assertEquals(T a, T b) {
+        if (a == null) {
+            if (b != null) {
+                throw new SanityCheckFailedException("\"" + a + "\" does not equal \"" + b + "\"");
+            }
+        } else if (!a.equals(b)) {
+            throw new SanityCheckFailedException("\"" + a + "\" does not equal \"" + b + "\"");
+        }
+    }
+
+    private static <T> void assertNotSame(T a, T b) {
+        if (a != b) {
+            throw new SanityCheckFailedException("\"" + a + "\" and \"" + b + "\" are not the same object");
+        }
+    }
+
+    private static void assertTrue(boolean condition) {
+        if (!condition) {
+            throw new SanityCheckFailedException("The condition is false");
+        }
+    }
+
+    private static void assertFalse(boolean condition) {
+        if (condition) {
+            throw new SanityCheckFailedException("The condition is true");
         }
     }
 }
