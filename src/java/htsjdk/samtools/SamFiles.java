062c8e79035a2fc2a484f5185b72ac0551d3ec8e
Vadim Zalunin <vadim.zalunin@gmail.com>
2015-08-10 15:36:35 +0100

    adding CRAM index file detection and tests

--- src/java/htsjdk/samtools/SamFiles.java
+++ src/java/htsjdk/samtools/SamFiles.java
@@ -6,8 +6,11 @@ import java.io.File;
  * @author mccowan
  */
 public class SamFiles {
+    public static final String CRAI_SUFFIX = ".crai";
+    public static final String CRAM_SUFFIX = ".cram";
+
     /**
-     * Finds the index file associated with the provided SAM file.  The index file must exist and be reachable to be found. 
+     * Finds the index file associated with the provided SAM file.  The index file must exist and be reachable to be found.
      *
      * @return The index for the provided SAM, or null if one was not found.
      */
@@ -18,17 +21,26 @@ public class SamFiles {
         if (fileName.endsWith(BamFileIoUtils.BAM_FILE_EXTENSION)) {
             final String bai = fileName.substring(0, fileName.length() - BamFileIoUtils.BAM_FILE_EXTENSION.length()) + BAMIndex.BAMIndexSuffix;
             indexFile = new File(samFile.getParent(), bai);
-            if (indexFile.isFile()) {
+            if (indexFile.isFile())
                 return indexFile;
-            }
+
+
+        } else if (fileName.endsWith(CRAM_SUFFIX)) {
+            final String crai = fileName.substring(0, fileName.length() - CRAM_SUFFIX.length()) + CRAI_SUFFIX;
+            indexFile = new File(samFile.getParent(), crai);
+            if (indexFile.isFile())
+                return indexFile;
+
+            indexFile = new File(samFile.getParent(), samFile.getName() + CRAI_SUFFIX);
+            if (indexFile.isFile())
+                return indexFile;
         }
 
         // If foo.bai doesn't exist look for foo.bam.bai
         indexFile = new File(samFile.getParent(), samFile.getName() + BAMIndex.BAMIndexSuffix);
-        if (indexFile.isFile()) {
+        if (indexFile.isFile())
             return indexFile;
-        } else {
-            return null;
-        }
+
+        return null;
     }
 }
