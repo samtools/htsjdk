1a8f6b633072574eaec2a37238aa9c7f28091f00
Vladimir Chugunov <vladimir_chugunov@epam.com>
2015-08-26 02:52:36 +0300

    NPE bug fix for BinningIndexContext for chucksless queries
    and the test files now
    
    Test fixed. Now it really do proper job.
    While splitting PR I've got that at some moment I broke my own test and it couldn't catch a bug I found: NPE was thrown only while reading plain .vcf files and it was OK while reading compessed VCF, e.g. .vcf.gz (which I've committed)
    
    So now this test does the following things:
    1)Creates plain-text '.vcf' VCF-file from compressed one ('.vcf.gz) and index for him
    2)Checks if .query method returns proper amount of VariantContext (wrapped in ClosableIterator)
    // Note: we're doing it for both plain and compressed file. The point of overworking with plain file is that I've already said: NPE was thrown only on this file type.
    
    Method contract restored. Null-check added
    
    Changes based on code review:
     * Replaced for-each loop with stream operation
     * Removed static imports from test class
     * Added 'final' modifiers for local variables in tests

--- src/tests/java/htsjdk/tribble/index/tabix/TabixIndexTest.java
+++ src/tests/java/htsjdk/tribble/index/tabix/TabixIndexTest.java
@@ -24,13 +24,20 @@
 package htsjdk.tribble.index.tabix;
 
 import htsjdk.samtools.util.BlockCompressedOutputStream;
+import htsjdk.tribble.index.IndexFactory;
 import htsjdk.tribble.util.LittleEndianOutputStream;
 import htsjdk.tribble.util.TabixUtils;
+import htsjdk.variant.variantcontext.VariantContext;
+import htsjdk.variant.variantcontext.writer.VariantContextWriter;
+import htsjdk.variant.variantcontext.writer.VariantContextWriterBuilder;
+import htsjdk.variant.vcf.VCFCodec;
+import htsjdk.variant.vcf.VCFFileReader;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.util.Iterator;
 
 public class TabixIndexTest {
     private static final File SMALL_TABIX_FILE = new File("testdata/htsjdk/tribble/tabix/trioDup.vcf.gz.tbi");
@@ -57,10 +64,80 @@ public class TabixIndexTest {
 
     @DataProvider(name = "readWriteTestDataProvider")
     public Object[][] readWriteTestDataProvider() {
-        return new Object[][] {
+        return new Object[][]{
                 {SMALL_TABIX_FILE},
                 {BIGGER_TABIX_FILE}
         };
     }
 
+    @Test
+    public void testQueryProvidedItemsAmount() {
+        final String VCF = "testdata/htsjdk/tribble/tabix/YRI.trio.2010_07.indel.sites.vcf";
+        // Note that we store only compressed files
+        final File plainTextVcfInputFile = new File(VCF);
+        plainTextVcfInputFile.deleteOnExit();
+        final File plainTextVcfIndexFile = new File(VCF + ".tbi");
+        plainTextVcfIndexFile.deleteOnExit();
+        final File compressedVcfInputFile = new File(VCF + ".gz");
+        final File compressedTbiIndexFile = new File(VCF + ".gz.tbi");
+        final VCFFileReader compressedVcfReader = new VCFFileReader(compressedVcfInputFile, compressedTbiIndexFile);
+
+        //create plain text VCF without "index on the fly" option
+        final VariantContextWriter plainTextVcfWriter = new VariantContextWriterBuilder()
+                .setOptions(VariantContextWriterBuilder.NO_OPTIONS)
+                .setOutputFile(VCF)
+                .build();
+        plainTextVcfWriter.writeHeader(compressedVcfReader.getFileHeader());
+        for (VariantContext vc : compressedVcfReader) {
+            if (vc != null) plainTextVcfWriter.add(vc);
+        }
+        plainTextVcfWriter.close();
+
+        IndexFactory.createTabixIndex(plainTextVcfInputFile,
+                new VCFCodec(),
+                TabixFormat.VCF,
+                new VCFFileReader(plainTextVcfInputFile, false).getFileHeader().getSequenceDictionary()
+        ) // create TabixIndex straight from plaintext VCF
+                .write(plainTextVcfIndexFile); // write it
+
+        final VCFFileReader plainTextVcfReader = new VCFFileReader(plainTextVcfInputFile, plainTextVcfIndexFile);
+        // Now we have both plaintext and compressed VCFs with provided TabixIndex-es and could test their "queryability"
+
+        // magic numbers chosen from just looking in provided VCF file
+        try {
+            // just somewhere in middle of chromosome
+            Assert.assertEquals(42, countIteratedElements(compressedVcfReader.query("1", 868379 - 1, 1006891 + 1)));
+            Assert.assertEquals(42, countIteratedElements(plainTextVcfReader.query("1", 868379 - 1, 1006891 + 1)));
+            // chromosome start
+            Assert.assertEquals(13, countIteratedElements(compressedVcfReader.query("1", 1, 836463 + 1)));
+            Assert.assertEquals(13, countIteratedElements(plainTextVcfReader.query("1", 1, 836463 + 1)));
+            // chromosome end
+            Assert.assertEquals(36, countIteratedElements(compressedVcfReader.query("1", 76690833 - 1, 76837502 + 11111111)));
+            Assert.assertEquals(36, countIteratedElements(plainTextVcfReader.query("1", 76690833 - 1, 76837502 + 11111111)));
+            // where's no one feature in the middle of chromosome
+            Assert.assertEquals(0, countIteratedElements(compressedVcfReader.query("1", 36606472 + 1, 36623523 - 1)));
+            Assert.assertEquals(0, countIteratedElements(plainTextVcfReader.query("1", 36606472 + 1, 36623523 - 1)));
+            // before chromosome
+            Assert.assertEquals(0, countIteratedElements(compressedVcfReader.query("1", 1, 10)));
+            Assert.assertEquals(0, countIteratedElements(plainTextVcfReader.query("1", 1, 10)));
+            // after chromosome
+            Assert.assertEquals(0, countIteratedElements(compressedVcfReader.query("1", 76837502 * 15, 76837502 * 16)));
+            Assert.assertEquals(0, countIteratedElements(plainTextVcfReader.query("1", 76837502 * 15, 76837502 * 16)));
+        } catch (NullPointerException e) {
+            Assert.fail("Exception caught on querying: ", e);
+            // before fix exception was thrown from 'TabixIndex.getBlocks()' on 'chunks.size()' while 'chunks == null' for plain files
+        } finally {
+            plainTextVcfReader.close();
+            compressedVcfReader.close();
+        }
+    }
+
+    private static int countIteratedElements(Iterator iterator) {
+        int counter = 0;
+        while (iterator.hasNext()) {
+            iterator.next();
+            counter++;
+        }
+        return counter;
+    }
 }
