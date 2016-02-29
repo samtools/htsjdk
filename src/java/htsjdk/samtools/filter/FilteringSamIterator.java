4f2e66dd884ff055a94d9492c182ee89a31e1978
Yossi Farjoun <farjoun@broadinstitute.org>
2016-02-29 17:27:35 -0500

    license

--- src/java/htsjdk/samtools/filter/FilteringSamIterator.java
+++ src/java/htsjdk/samtools/filter/FilteringSamIterator.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright (c) 2009 The Broad Institute
+ * Copyright (c) 2016 The Broad Institute
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -21,6 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
 package htsjdk.samtools.filter;
 
 import htsjdk.samtools.SAMFileHeader;
