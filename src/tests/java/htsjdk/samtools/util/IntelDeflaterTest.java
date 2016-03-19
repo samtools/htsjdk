15c4618c7c8bd52dfcfc6ae97af84e7a1f216d24
Yossi Farjoun <farjoun@gmail.com>
2016-03-19 00:31:32 -0400

    making test more like a unit test

---
+++ src/tests/java/htsjdk/samtools/util/IntelDeflaterTest.java
@@ -0,0 +1,123 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2016 The Broad Institute
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ */
+package htsjdk.samtools.util;
+
+import htsjdk.samtools.Defaults;
+import htsjdk.samtools.SAMFileHeader;
+import htsjdk.samtools.SAMFileWriter;
+import htsjdk.samtools.SAMFileWriterFactory;
+import htsjdk.samtools.SAMRecord;
+import htsjdk.samtools.SamReader;
+import htsjdk.samtools.SamReaderFactory;
+import htsjdk.samtools.ValidationStringency;
+import htsjdk.samtools.util.zip.DeflaterFactory;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * This is a test for IntelDeflater.
+ */
+
+public class IntelDeflaterTest {
+    static final File TEST_DIR = new File("testdata/htsjdk/samtools");
+
+    @DataProvider(name="TestIntelDeflatorIsLoadedData")
+    Iterator<Object[]> TestIntelDeflatorIsLoadedData(){
+
+        List<File> files = CollectionUtil.makeList(
+                new File(TEST_DIR, "coordinate_sorted.sam"),
+                new File(TEST_DIR, "queryname_sorted.sam"),
+                new File(TEST_DIR, "compressed.bam"),
+                new File(TEST_DIR, "empty.bam"),
+                new File(TEST_DIR, "cram_with_bai_index.cram"),
+                new File(TEST_DIR, "uncompressed.sam"),
+                new File(TEST_DIR, "cram_with_crai_index.cram"));
+
+        List<Boolean> eagerlyDecodes = CollectionUtil.makeList(Boolean.TRUE, Boolean.FALSE);
+        List<Integer> compressionLevels = CollectionUtil.makeList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+        List<Object[]> retVal = new ArrayList<>();
+        files.stream()
+                .forEach(file ->
+                        eagerlyDecodes.stream()
+                                .forEach(eagerlyDecode -> compressionLevels.stream()
+                                        .forEach(compressionLevel ->
+                                                retVal.add(new Object[]{file, eagerlyDecode, compressionLevel}))));
+        return retVal.iterator();
+    }
+
+    @Test(dataProvider = "TestIntelDeflatorIsLoadedData", groups="intel")
+    public void TestIntelDeflatorIsLoaded(final File inputFile, final Boolean eagerlyDecode,final Integer compressionLevel) throws IOException {
+
+        System.err.print("In TestIntelDeflatorIsLoaded");
+        IOUtil.assertFileIsReadable(inputFile);
+        System.err.flush();//make sure that this is visible if any error occurs after this point.
+
+        final File outputFile = File.createTempFile("IndelDeflator", "bam");
+        outputFile.deleteOnExit();
+
+
+        Assert.assertTrue(DeflaterFactory.usingIntelDeflater(), "IntelDeflator is not loaded.");
+        System.err.print("IntelDeflator is loaded");
+        System.err.flush();//make sure that this is visible if any error occurs after this point.
+
+
+        SamReaderFactory readerFactory = SamReaderFactory.makeDefault().validationStringency(ValidationStringency.SILENT);
+        if (eagerlyDecode) {
+            readerFactory = readerFactory.enable(SamReaderFactory.Option.EAGERLY_DECODE);
+        }
+
+        if(inputFile.getName().endsWith(".cram")) {
+            readerFactory.referenceSequence(new File(TEST_DIR, "hg19mini.fasta"));
+        }
+
+        final SamReader reader = readerFactory.open(inputFile);
+        final SAMFileHeader header = reader.getFileHeader();
+        final SAMFileWriter writer = new SAMFileWriterFactory().makeBAMWriter(header, true, outputFile, compressionLevel);
+        int nRecords = 0;
+        for (final SAMRecord record : reader) {
+            writer.addAlignment(record);
+            nRecords++;
+        }
+        writer.close();
+
+        int nReadRecords = 0;
+        try (final SamReader outputReader = readerFactory.open(outputFile)) {
+            for (final SAMRecord ignored : outputReader) {
+                nReadRecords++;
+            }
+        } catch (Exception e) {
+            Assert.fail("Error reading record written with the IntelDeflater library");
+        }
+        Assert.assertEquals(nReadRecords, nRecords, "Number of read records mismatches number of written records.");
+    }
+}
