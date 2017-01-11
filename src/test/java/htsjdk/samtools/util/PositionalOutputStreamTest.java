5a9d819edceb563b99b900049b2dab4a9061a68e
Daniel Gómez-Sánchez <magicDGS@users.noreply.github.com>
2017-01-11 16:15:35 +0100

    fixed unmappable character for encoding ASCII (#784)

--- src/test/java/htsjdk/samtools/util/PositionalOutputStreamTest.java
+++ src/test/java/htsjdk/samtools/util/PositionalOutputStreamTest.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2015 Daniel Gómez-Sánchez
+ * Copyright (c) 2015 Daniel Gomez-Sanchez
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
