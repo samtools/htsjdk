c4270941d946c430c08dc046289d63acb0753c28
Daniel Gómez-Sánchez <daniel.gomez.sanchez@hotmail.es>
2017-03-01 15:35:04 +0100

    FeatureWriter implementations
    - Encoder-based implementation
    - Implementation for indexing on the fly

---
+++ src/main/java/htsjdk/tribble/writer/IndexingFeatureWriter.java
@@ -0,0 +1,118 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017 Daniel Gomez-Sanchez
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package htsjdk.tribble.writer;
+
+import htsjdk.samtools.SAMSequenceDictionary;
+import htsjdk.samtools.util.BlockCompressedOutputStream;
+import htsjdk.samtools.util.IOUtil;
+import htsjdk.samtools.util.LocationAware;
+import htsjdk.samtools.util.PositionalOutputStream;
+import htsjdk.tribble.Feature;
+import htsjdk.tribble.index.Index;
+import htsjdk.tribble.index.IndexCreator;
+import htsjdk.tribble.index.tabix.TabixIndex;
+import htsjdk.tribble.util.LittleEndianOutputStream;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+
+/**
+ * Feature writer class for indexing on the fly.
+ *
+ * @param <F> feature type.
+ *
+ * @author Daniel Gomez-Sanchez (magicDGS)
+ */
+final class IndexingFeatureWriter<F extends Feature> extends FeatureWriterImpl<F> {
+
+    private final LocationAware location;
+    private final IndexCreator indexer;
+    private final String indexPath;
+    private final SAMSequenceDictionary refDict;
+
+    /**
+     * Construct a new indexing feature writer.
+     *
+     * @param encoder      encoder for features.
+     * @param outputStream the underlying output stream.
+     * @param idxCreator   indexer.
+     * @param indexPath    the file to write the index.
+     * @param refDict      dictionary to write in the index. May be {@code null}.
+     */
+    public IndexingFeatureWriter(final FeatureEncoder<F> encoder, final OutputStream outputStream,
+            final IndexCreator idxCreator, final String indexPath,
+            final SAMSequenceDictionary refDict) {
+        super(encoder, asLocationAwareStream(outputStream));
+        this.indexer = idxCreator;
+        this.indexPath = indexPath;
+        this.refDict = refDict;
+        // this is already a LocationAware
+        this.location = (LocationAware) getOutputStream();
+    }
+
+    /** Wraps if necessary the output stream. */
+    private static OutputStream asLocationAwareStream(final OutputStream outputStream) {
+        return (outputStream instanceof LocationAware)
+                ? outputStream : new PositionalOutputStream(outputStream);
+    }
+
+    /**
+     * Adds the feature to the indexer, and then write using the {@link FeatureWriterImpl}
+     * implementation.
+     */
+    @Override
+    public void add(final F feature) throws IOException {
+        // should be added to the indexer first
+        indexer.addFeature(feature, location.getPosition());
+        super.add(feature);
+    }
+
+    /** Adds the reference dictionary (if not {@code null}), finalize/write the index and close. */
+    @Override
+    public void close() throws IOException {
+        if (refDict != null) {
+            indexer.setIndexSequenceDictionary(refDict);
+        }
+        final Index index = indexer.finalizeIndex(location.getPosition());
+        // TODO: uncomment next line after https://github.com/samtools/htsjdk/pull/810
+        // index.write(indexPath);
+        writeIndex(index);
+        super.close();
+    }
+
+    // TODO: remove comment and usage after https://github.com/samtools/htsjdk/pull/810
+    private final void writeIndex(final Index index) throws IOException {
+        OutputStream outputStream = Files.newOutputStream(IOUtil.getPath(indexPath));
+        if (index instanceof TabixIndex) {
+            outputStream = new BlockCompressedOutputStream(outputStream, null);
+        } else {
+            outputStream = new BufferedOutputStream(outputStream);
+        }
+        index.write(new LittleEndianOutputStream(outputStream));
+        outputStream.close();
+    }
+}
