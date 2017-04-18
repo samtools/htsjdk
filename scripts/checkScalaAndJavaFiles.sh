83904daa28b3ec325243452c4c4a02efc21dc253
Yossi Farjoun <farjoun@gmail.com>
2017-04-18 14:18:56 -0400

    responding to verbal review comments

--- scripts/checkScalaAndJavaFiles.sh
+++ scripts/checkScalaAndJavaFiles.sh
@@ -1,5 +1,8 @@
 #/bin/bash
 
+# Check that Scala files only exist in the scala test dir and
+# that java files do not reside in the scala test dir
+
 if `find src | grep -v '^src/test/scala' | grep -q '\.scala$' ` ; then
 	echo 'Found scala file(s) outside of scala test directory';
 	find src | grep -v '^src/test/scala' | grep '\.scala$'
