b73d690c579cca77732fb449a621273b8003c269
Nils Homer <nilshomer@gmail.com>
2017-05-20 22:05:43 -0700

    tabs tabs everywhere

--- src/test/java/htsjdk/variant/vcf/VCFEncoderTest.java
+++ src/test/java/htsjdk/variant/vcf/VCFEncoderTest.java
@@ -21,9 +21,9 @@ import java.util.TreeSet;
 
 public class VCFEncoderTest extends HtsjdkTest {
 
-	@DataProvider(name = "VCFWriterDoubleFormatTestData")
-	public Object[][] makeVCFWriterDoubleFormatTestData() {
-		final List<Object[]> tests = new ArrayList<Object[]>();
+    @DataProvider(name = "VCFWriterDoubleFormatTestData")
+    public Object[][] makeVCFWriterDoubleFormatTestData() {
+        final List<Object[]> tests = new ArrayList<Object[]>();
         // New spec test cases
         tests.add(new Object[]{0.0, "0.0"});
         tests.add(new Object[]{1.0, "1.0"});
@@ -38,36 +38,36 @@ public class VCFEncoderTest extends HtsjdkTest {
         tests.add(new Object[]{0.50000001, "0.5"});
         tests.add(new Object[]{0.012345, "1.23450e-02"});
         tests.add(new Object[]{0.0033333333, "3.33333e-03"});
-		// Previous test cases
-		tests.add(new Object[]{1.0, "1.0"});
-		tests.add(new Object[]{10.1, "10.1"});
-		tests.add(new Object[]{10.01, "10.01"});
-		tests.add(new Object[]{10.012, "10.012"});
-		tests.add(new Object[]{10.015, "10.015"});
-		tests.add(new Object[]{0.0, "0.0"});
-		tests.add(new Object[]{0.5, "0.5"});
-		tests.add(new Object[]{0.55, "0.55"});
-		tests.add(new Object[]{0.555, "0.555"});
-		tests.add(new Object[]{0.5555, "0.5555"});
-		tests.add(new Object[]{0.1, "0.1"});
-		tests.add(new Object[]{0.050, "0.05"});
-		tests.add(new Object[]{0.010, "0.01"});
-		tests.add(new Object[]{0.012, "1.20000e-02"});
-		tests.add(new Object[]{0.0012, "1.20000e-03"});
-		tests.add(new Object[]{1.2e-4, "1.20000e-04"});
-		tests.add(new Object[]{1.21e-4, "1.21000e-04"});
-		tests.add(new Object[]{1.212e-5, "1.21200e-05"});
-		tests.add(new Object[]{1.2123e-6, "1.21230e-06"});
-		tests.add(new Object[]{Double.POSITIVE_INFINITY, "Infinity"});
-		tests.add(new Object[]{Double.NEGATIVE_INFINITY, "-Infinity"});
-		tests.add(new Object[]{Double.NaN, "NaN"});
+        // Previous test cases
+        tests.add(new Object[]{1.0, "1.0"});
+        tests.add(new Object[]{10.1, "10.1"});
+        tests.add(new Object[]{10.01, "10.01"});
+        tests.add(new Object[]{10.012, "10.012"});
+        tests.add(new Object[]{10.015, "10.015"});
+        tests.add(new Object[]{0.0, "0.0"});
+        tests.add(new Object[]{0.5, "0.5"});
+        tests.add(new Object[]{0.55, "0.55"});
+        tests.add(new Object[]{0.555, "0.555"});
+        tests.add(new Object[]{0.5555, "0.5555"});
+        tests.add(new Object[]{0.1, "0.1"});
+        tests.add(new Object[]{0.050, "0.05"});
+        tests.add(new Object[]{0.010, "0.01"});
+        tests.add(new Object[]{0.012, "1.20000e-02"});
+        tests.add(new Object[]{0.0012, "1.20000e-03"});
+        tests.add(new Object[]{1.2e-4, "1.20000e-04"});
+        tests.add(new Object[]{1.21e-4, "1.21000e-04"});
+        tests.add(new Object[]{1.212e-5, "1.21200e-05"});
+        tests.add(new Object[]{1.2123e-6, "1.21230e-06"});
+        tests.add(new Object[]{Double.POSITIVE_INFINITY, "Infinity"});
+        tests.add(new Object[]{Double.NEGATIVE_INFINITY, "-Infinity"});
+        tests.add(new Object[]{Double.NaN, "NaN"});
         return tests.toArray(new Object[][]{});
-	}
+    }
 
-	@Test(dataProvider = "VCFWriterDoubleFormatTestData")
-	public void testVCFWriterDoubleFormatTestData(final double d, final String expected) {
-		Assert.assertEquals(VCFEncoder.formatVCFDouble(d), expected, "Failed to pretty print double in VCFWriter");
-	}
+    @Test(dataProvider = "VCFWriterDoubleFormatTestData")
+    public void testVCFWriterDoubleFormatTestData(final double d, final String expected) {
+        Assert.assertEquals(VCFEncoder.formatVCFDouble(d), expected, "Failed to pretty print double in VCFWriter");
+    }
 
     @DataProvider(name = "MissingFormatTestData")
     public Object[][] makeMissingFormatTestData() {
