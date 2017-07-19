7ab3fc8a0e0f8a595a6574d96c3bbd27aece45ec
Daniel Gómez-Sánchez <daniel.gomez.sanchez@hotmail.es>
2017-07-19 19:15:33 +0200

    Full coverage for CigarElement

--- src/test/java/htsjdk/samtools/util/CigarElementUnitTest.java
+++ src/test/java/htsjdk/samtools/util/CigarElementUnitTest.java
@@ -4,6 +4,8 @@ package htsjdk.samtools.util;
 import htsjdk.HtsjdkTest;
 import htsjdk.samtools.CigarElement;
 import htsjdk.samtools.CigarOperator;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class CigarElementUnitTest extends HtsjdkTest {
@@ -11,5 +13,29 @@ public class CigarElementUnitTest extends HtsjdkTest {
     @Test(expectedExceptions = IllegalArgumentException.class)
     public void testNegativeLengthCheck(){
         final CigarElement element = new CigarElement(-1, CigarOperator.M);
+    }
+
+
+    @DataProvider
+    public Object[][] elementsForEquals() {
+        final CigarElement mElement = new CigarElement(10, CigarOperator.M);
+        return new Object[][] {
+                // same object
+                {mElement, mElement, true},
+                // different equal objects
+                {mElement, new CigarElement(mElement.getLength(), mElement.getOperator()), true},
+                // different lengths
+                {mElement, new CigarElement(mElement.getLength() + 1, mElement.getOperator()), false},
+                // different operators
+                {mElement, new CigarElement(mElement.getLength(), CigarOperator.X), false},
+                // different class
+                {mElement, mElement.toString(), false}
+        };
+    }
+
+    @Test(dataProvider = "elementsForEquals")
+    public void testEqualsAndHashCode(final CigarElement element, final Object other, final boolean isEquals) {
+        Assert.assertEquals(element.equals(other), isEquals);
+        Assert.assertEquals(element.hashCode() == other.hashCode(), isEquals);
     }
 }
