da51f883977f3dc32bce6efb713c4dfc0e4bea87
Daniel Gómez-Sánchez <daniel.gomez.sanchez@hotmail.es>
2017-07-27 15:30:42 +0200

    Increase test coverage for SAMReadGroupRecord

--- src/test/java/htsjdk/samtools/SAMReadGroupRecordTest.java
+++ src/test/java/htsjdk/samtools/SAMReadGroupRecordTest.java
@@ -24,8 +24,14 @@
 package htsjdk.samtools;
 
 import htsjdk.HtsjdkTest;
+import htsjdk.samtools.util.Iso8601Date;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.util.Date;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
 
 /**
  * Test for SAMReadGroupRecordTest
@@ -40,4 +46,74 @@ public class SAMReadGroupRecordTest extends HtsjdkTest {
         r.setDescription("my description");
         Assert.assertEquals("@RG\tID:rg1\tSM:mysample\tPL:ILLUMINA\tDS:my description", r.getSAMString());
     }
+
+    @Test
+    public void testGettersAndSetters() throws Exception {
+        final SAMReadGroupRecord rg = new SAMReadGroupRecord("rg1");
+        // test the ID from the two getters
+        Assert.assertEquals(rg.getId(), "rg1");
+        Assert.assertEquals(rg.getReadGroupId(), "rg1");
+
+        // test that the rest of getters returns null if unset
+        testGetterAndSetter(rg, "sample", SAMReadGroupRecord::setSample, SAMReadGroupRecord::getSample);
+        testGetterAndSetter(rg, "library", SAMReadGroupRecord::setLibrary, SAMReadGroupRecord::getLibrary);
+        testGetterAndSetter(rg, "library", SAMReadGroupRecord::setPlatformUnit, SAMReadGroupRecord::getPlatformUnit);
+        testGetterAndSetter(rg, "platform", SAMReadGroupRecord::setPlatform, SAMReadGroupRecord::getPlatform);
+        testGetterAndSetter(rg, new Iso8601Date(new Date()), SAMReadGroupRecord::setRunDate, SAMReadGroupRecord::getRunDate);
+        testGetterAndSetter(rg, "flow_order", SAMReadGroupRecord::setFlowOrder, SAMReadGroupRecord::getFlowOrder);
+        testGetterAndSetter(rg, "key_sequence", SAMReadGroupRecord::setKeySequence, SAMReadGroupRecord::getKeySequence);
+        testGetterAndSetter(rg, "sequencing_center", SAMReadGroupRecord::setSequencingCenter, SAMReadGroupRecord::getSequencingCenter);
+        testGetterAndSetter(rg, "description", SAMReadGroupRecord::setDescription, SAMReadGroupRecord::getDescription);
+        testGetterAndSetter(rg, 10, SAMReadGroupRecord::setPredictedMedianInsertSize, SAMReadGroupRecord::getPredictedMedianInsertSize);
+        testGetterAndSetter(rg, "program_group", SAMReadGroupRecord::setProgramGroup, SAMReadGroupRecord::getProgramGroup);
+        testGetterAndSetter(rg, "platform_model", SAMReadGroupRecord::setPlatformModel, SAMReadGroupRecord::getPlatformModel);
+    }
+
+    private static <T> void testGetterAndSetter(final SAMReadGroupRecord record, final T value,
+            final BiConsumer<SAMReadGroupRecord, T> setter,
+            final Function<SAMReadGroupRecord, T> getter) {
+        Assert.assertNull(getter.apply(record));
+        setter.accept(record, value);
+        Assert.assertEquals(getter.apply(record), value);
+        setter.accept(record, null);
+        Assert.assertNull(getter.apply(record));
+    }
+
+    @Test
+    public void testSetNonIso8601Date() throws Exception {
+        final SAMReadGroupRecord rg = new SAMReadGroupRecord("rg1");
+        // set not ISO 8601 date
+        final Date date = new Date();
+        rg.setRunDate(date);
+        // and assert that it is correctly wrapped
+        Assert.assertEquals(rg.getRunDate(), new Iso8601Date(date));
+    }
+
+
+    @DataProvider
+    public Object[][] readGroupsForEquals() {
+        final SAMReadGroupRecord empty = new SAMReadGroupRecord("empty");
+        final SAMReadGroupRecord withSample = new SAMReadGroupRecord("rg1");
+        withSample.setSample("sample1");
+        return new Object[][] {
+                // same object
+                {empty, empty, true},
+                {withSample, withSample, true},
+                // null or different class
+                {empty, null, false},
+                {empty, empty.getId(), false},
+                // different information set
+                {empty, withSample, false},
+                {withSample, empty, false},
+        };
+    }
+
+    @Test(dataProvider = "readGroupsForEquals")
+    public void testEqualsAndHashcode(final SAMReadGroupRecord rg, final Object other, final boolean isEqual) throws Exception {
+        Assert.assertEquals(rg.equals(other), isEqual);
+        if (isEqual) {
+            Assert.assertEquals(rg.hashCode(), other.hashCode());
+        }
+    }
+
 }
