26eada2f0f542aa65127991e47026a1b1fd6abc5
Yossi Farjoun <farjoun@broadinstitute.org>
2017-10-07 18:57:32 +0300

    Fixed ENA url to use https (instead of http)  (#1004)
    - it seems that the ENA has moved to https which is why tests are failing. fixed.
    - reformatted tests to use a data provider
     - added link to page where URL is (now) defined properly

--- src/test/java/htsjdk/samtools/cram/ref/EnaRefServiceTest.java
+++ src/test/java/htsjdk/samtools/cram/ref/EnaRefServiceTest.java
@@ -2,15 +2,21 @@ package htsjdk.samtools.cram.ref;
 
 import htsjdk.HtsjdkTest;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 
 public class EnaRefServiceTest extends HtsjdkTest {
 
-    @Test
-    public void test() throws IOException, EnaRefService.GaveUpException {
-        Assert.assertNotNull(new EnaRefService().getSequence("57151e6196306db5d9f33133572a5482"));
-        Assert.assertNotNull(new EnaRefService().getSequence("0000088cbcebe818eb431d58c908c698"));
+    @DataProvider(name="testEnaRefServiceData")
+    public Object[][] testEnaRefServiceData(){
+        return new Object[][]{{"57151e6196306db5d9f33133572a5482"},
+                {"0000088cbcebe818eb431d58c908c698"}};
+    }
+
+    @Test(dataProvider = "testEnaRefServiceData")
+    public void testEnaRefServiceData(final String md5) throws IOException, EnaRefService.GaveUpException {
+        Assert.assertNotNull(new EnaRefService().getSequence(md5));
     }
 }
