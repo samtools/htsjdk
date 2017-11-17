samtools/htsjdk

commit 0ab0667c9eb7987eaf73e5add7e91098e1feacdf

More support for java.nio.Path: DiskBackedQueue and SortingCollection  (#910)
* More support for java.nio.Path: DiskBackedQueue and SortingCollection

- New methods in IOUtil to support Paths
- Change DiskBackedQueue to use Paths
- Change SortingCollection to use Paths
- Change SortingLongCollection to use Paths

src/main/java/htsjdk/samtools/util/IOUtil.java
@@ -69,7 +69,6 @@ import java.util.Stack;
 import java.util.regex.Pattern;
 import java.util.zip.Deflater;
 import java.util.zip.GZIPInputStream;
-import java.util.zip.GZIPOutputStream;
 
 /**
  * Miscellaneous stateless static IO-oriented methods.
@@ -238,6 +237,19 @@ public class IOUtil {
         }
     }
 
+    public static void deletePaths(final Path... paths) {
+        deletePaths(Arrays.asList(paths));
+    }
+
+    public static void deletePaths(final Iterable<Path> paths) {
+        for (final Path p : paths) {
+            try {
+                Files.delete(p);
+            } catch (IOException e) {
+                System.err.println("Could not delete file " + p);
+            }
+        }
+    }
 
     /**
      * @return true if the path is not a device (e.g. /dev/null or /dev/stdin), and is not
@@ -282,7 +294,6 @@ public class IOUtil {
         return newTempFile(prefix, suffix, tmpDirs, FIVE_GBS);
     }
 
-
     /** Returns a default tmp directory. */
     public static File getDefaultTmpDir() {
         final String user = System.getProperty("user.name");
@@ -290,6 +301,79 @@ public class IOUtil {
 
         if (tmp.endsWith(File.separatorChar + user)) return new File(tmp);
         else return new File(tmp, user);
+    }
+
+    /**
+     * Creates a new tmp path on one of the available temp filesystems, registers it for deletion
+     * on JVM exit and then returns it.
+     */
+    public static Path newTempPath(final String prefix, final String suffix,
+            final Path[] tmpDirs, final long minBytesFree) throws IOException {
+        Path p = null;
+
+        for (int i = 0; i < tmpDirs.length; ++i) {
+            if (i == tmpDirs.length - 1 || Files.getFileStore(tmpDirs[i]).getUsableSpace() > minBytesFree) {
+                p = Files.createTempFile(tmpDirs[i], prefix, suffix);
+                deleteOnExit(p);
+                break;
+            }
+        }
+
+        return p;
+    }
+
+    /** Creates a new tmp file on one of the potential filesystems that has at least 5GB free. */
+    public static Path newTempPath(final String prefix, final String suffix,
+            final Path[] tmpDirs) throws IOException {
+        return newTempPath(prefix, suffix, tmpDirs, FIVE_GBS);
+    }
+
+    /** Returns a default tmp directory as a Path. */
+    public static Path getDefaultTmpDirPath() {
+        try {
+            final String user = System.getProperty("user.name");
+            final String tmp = System.getProperty("java.io.tmpdir");
+
+            final Path tmpParent = getPath(tmp);
+            if (tmpParent.endsWith(tmpParent.getFileSystem().getSeparator() + user)) {
+                return tmpParent;
+            } else {
+                return tmpParent.resolve(user);
+            }
+        } catch (IOException e) {
+            throw new RuntimeIOException(e);
+        }
+    }
+
+    /**
+     * Deletes on exit a path by creating a shutdown hook.
+     */
+    public static void deleteOnExit(final Path path) {
+        // add a shutdown hook to remove the path on exit
+        Runtime.getRuntime().addShutdownHook(new DeletePathThread(path));
+    }
+
+    /**
+     * WARNING: visible for testing. Do not use.
+     *
+     * Class for delete a path, used in a shutdown hook for delete on exit.
+     *
+     * @see #deleteOnExit(Path)
+     */
+    protected static final class DeletePathThread extends Thread {
+
+        private final Path path;
+
+        protected DeletePathThread(Path path) {this.path = path;}
+
+        @Override
+        public void run() {
+            try {
+                Files.delete(path);
+            } catch (IOException e) {
+                throw new RuntimeIOException(e);
+            }
+        }
     }
 
     /** Returns the name of the file minus the extension (i.e. text after the last "." in the filename). */
@@ -436,17 +520,28 @@ public class IOUtil {
      * @param dir the dir to check for writability
      */
     public static void assertDirectoryIsWritable(final File dir) {
+        final Path asPath = (dir == null) ? null : dir.toPath();
+        assertDirectoryIsWritable(asPath);
+    }
+
+    /**
+     * Checks that a directory is non-null, extent, writable and a directory
+     * otherwise a runtime exception is thrown.
+     *
+     * @param dir the dir to check for writability
+     */
+    public static void assertDirectoryIsWritable(final Path dir) {
         if (dir == null) {
             throw new IllegalArgumentException("Cannot check readability of null file.");
         }
-        else if (!dir.exists()) {
-            throw new SAMException("Directory does not exist: " + dir.getAbsolutePath());
+        else if (!Files.exists(dir)) {
+            throw new SAMException("Directory does not exist: " + dir.toUri().toString());
         }
-        else if (!dir.isDirectory()) {
-            throw new SAMException("Cannot write to directory because it is not a directory: " + dir.getAbsolutePath());
+        else if (!Files.isDirectory(dir)) {
+            throw new SAMException("Cannot write to directory because it is not a directory: " + dir.toUri().toString());
         }
-        else if (!dir.canWrite()) {
-            throw new SAMException("Directory exists but is not writable: " + dir.getAbsolutePath());
+        else if (!Files.isWritable(dir)) {
+            throw new SAMException("Directory exists but is not writable: " + dir.toUri().toString());
         }
     }
 

src/main/java/htsjdk/samtools/util/DiskBackedQueue.java
@@ -28,17 +28,18 @@ import htsjdk.samtools.Defaults;
 import htsjdk.samtools.SAMException;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayDeque;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Queue;
+import java.util.stream.Collectors;
 
 /**
  * A single-ended FIFO queue. Writes elements to temporary files when the queue gets too big.
@@ -56,7 +57,7 @@ import java.util.Queue;
 public class DiskBackedQueue<E> implements Queue<E> {
     private final int maxRecordsInRamQueue;
     private final Queue<E> ramRecords;
-    private File diskRecords = null;
+    private Path diskRecords = null;
     private final TempStreamFactory tempStreamFactory = new TempStreamFactory();
     private OutputStream outputStream = null;
     private InputStream inputStream = null;
@@ -67,7 +68,7 @@ public class DiskBackedQueue<E> implements Queue<E> {
     private E headRecord = null;
 
     /** Directories where file of records go. **/
-    private final List<File> tmpDirs;
+    private final List<Path> tmpDirs;
 
     /**
      * Used to write records to file, and used as a prototype to create codecs for reading.
@@ -83,14 +84,14 @@ public class DiskBackedQueue<E> implements Queue<E> {
      * @param tmpDirs Where to write files of records that will not fit in RAM
      */
     private DiskBackedQueue(final SortingCollection.Codec<E> codec,
-                            final int maxRecordsInRam, final List<File> tmpDirs) {
+                            final int maxRecordsInRam, final List<Path> tmpDirs) {
         if (maxRecordsInRam < 0) {
             throw new IllegalArgumentException("maxRecordsInRamQueue must be >= 0");
         }
         if (tmpDirs == null || tmpDirs.isEmpty()) {
             throw new IllegalArgumentException("At least one temp directory must be provided.");
         }
-        for (final File tmpDir : tmpDirs) IOUtil.assertDirectoryIsWritable(tmpDir);
+        for (final Path tmpDir : tmpDirs) IOUtil.assertDirectoryIsWritable(tmpDir);
         this.tmpDirs = tmpDirs;
         this.codec = codec;
         this.maxRecordsInRamQueue = (maxRecordsInRam == 0) ? 0 : maxRecordsInRam - 1; // the first of our ram records is stored as headRecord
@@ -107,6 +108,19 @@ public class DiskBackedQueue<E> implements Queue<E> {
     public static <T> DiskBackedQueue<T> newInstance(final SortingCollection.Codec<T> codec,
                                                      final int maxRecordsInRam,
                                                      final List<File> tmpDir) {
+        return new DiskBackedQueue<T>(codec, maxRecordsInRam, tmpDir.stream().map(File::toPath).collect(Collectors.toList()));
+    }
+
+    /**
+     * Syntactic sugar around the ctor, to save some typing of type parameters
+     *
+     * @param codec For writing records to file and reading them back into RAM
+     * @param maxRecordsInRam how many records to accumulate in memory before spilling to disk
+     * @param tmpDir Where to write files of records that will not fit in RAM
+     */
+    public static <T> DiskBackedQueue<T> newInstanceFromPaths(final SortingCollection.Codec<T> codec,
+            final int maxRecordsInRam,
+            final List<Path> tmpDir) {
         return new DiskBackedQueue<T>(codec, maxRecordsInRam, tmpDir);
     }
 
@@ -254,7 +268,7 @@ public class DiskBackedQueue<E> implements Queue<E> {
         try {
             if (this.diskRecords == null) {
                 this.diskRecords = newTempFile();
-                this.outputStream = tempStreamFactory.wrapTempOutputStream(new FileOutputStream(this.diskRecords), Defaults.BUFFER_SIZE);
+                this.outputStream = tempStreamFactory.wrapTempOutputStream(Files.newOutputStream(this.diskRecords), Defaults.BUFFER_SIZE);
                 this.codec.setOutputStream(this.outputStream);
             }
             this.codec.encode(record);
@@ -269,8 +283,8 @@ public class DiskBackedQueue<E> implements Queue<E> {
      * Creates a new tmp file on one of the available temp filesystems, registers it for deletion
      * on JVM exit and then returns it.
      */
-    private File newTempFile() throws IOException {
-        return IOUtil.newTempFile("diskbackedqueue.", ".tmp", this.tmpDirs.toArray(new File[tmpDirs.size()]), IOUtil.FIVE_GBS);
+    private Path newTempFile() throws IOException {
+        return IOUtil.newTempPath("diskbackedqueue.", ".tmp", this.tmpDirs.toArray(new Path[tmpDirs.size()]), IOUtil.FIVE_GBS);
     }
 
     /**
@@ -300,7 +314,7 @@ public class DiskBackedQueue<E> implements Queue<E> {
      * @return The next element from the head of the file, or null if end-of-file is reached
      * @throws RuntimeIOException
      */
-    private E readFileRecord (final File file) {
+    private E readFileRecord (final Path file) {
         if (this.canAdd) this.canAdd = false; // NB: should this just be an assignment regardless?
 
         // we never wrote a record to disk
@@ -309,7 +323,7 @@ public class DiskBackedQueue<E> implements Queue<E> {
         }
         try {
             if (this.inputStream == null) {
-                inputStream = new FileInputStream(file);
+                inputStream = Files.newInputStream(file);
                 this.codec.setInputStream(tempStreamFactory.wrapTempInputStream(inputStream, Defaults.BUFFER_SIZE));
             }
             final E record = this.codec.decode(); // NB: returns null if end-of-file is reached.
@@ -325,7 +339,7 @@ public class DiskBackedQueue<E> implements Queue<E> {
     private void closeIOResources() {
         CloserUtil.close(this.outputStream);
         CloserUtil.close(this.inputStream);
-        if (this.diskRecords != null) IOUtil.deleteFiles(this.diskRecords);
+        if (this.diskRecords != null) IOUtil.deletePaths(this.diskRecords);
     }
 
     /**

src/main/java/htsjdk/samtools/util/SortingCollection.java
@@ -26,14 +26,14 @@ package htsjdk.samtools.util;
 import htsjdk.samtools.Defaults;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.Serializable;
 import java.lang.reflect.Array;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -96,7 +96,7 @@ public class SortingCollection<T> implements Iterable<T> {
     }
 
     /** Directories where files of sorted records go. */
-    private final File[] tmpDirs;
+    private final Path[] tmpDirs;
 
     /** The minimum amount of space free on a temp filesystem to write a file there. */
     private final long TMP_SPACE_FREE = IOUtil.FIVE_GBS;
@@ -124,7 +124,7 @@ public class SortingCollection<T> implements Iterable<T> {
     /**
      * List of files in tmpDir containing sorted records
      */
-    private final List<File> files = new ArrayList<File>();
+    private final List<Path> files = new ArrayList<>();
 
     private boolean destructiveIteration = true;
 
@@ -139,7 +139,7 @@ public class SortingCollection<T> implements Iterable<T> {
      * @param tmpDir Where to write files of records that will not fit in RAM
      */
     private SortingCollection(final Class<T> componentType, final SortingCollection.Codec<T> codec,
-                             final Comparator<T> comparator, final int maxRecordsInRam, final File... tmpDir) {
+                             final Comparator<T> comparator, final int maxRecordsInRam, final Path... tmpDir) {
         if (maxRecordsInRam <= 0) {
             throw new IllegalArgumentException("maxRecordsInRam must be > 0");
         }
@@ -217,10 +217,10 @@ public class SortingCollection<T> implements Iterable<T> {
     private void spillToDisk() {
         try {
             Arrays.sort(this.ramRecords, 0, this.numRecordsInRam, this.comparator);
-            final File f = newTempFile();
+            final Path f = newTempFile();
             OutputStream os = null;
             try {
-                os = tempStreamFactory.wrapTempOutputStream(new FileOutputStream(f), Defaults.BUFFER_SIZE);
+                os = tempStreamFactory.wrapTempOutputStream(Files.newOutputStream(f), Defaults.BUFFER_SIZE);
                 this.codec.setOutputStream(os);
                 for (int i = 0; i < this.numRecordsInRam; ++i) {
                     this.codec.encode(ramRecords[i]);
@@ -230,7 +230,7 @@ public class SortingCollection<T> implements Iterable<T> {
 
                 os.flush();
             } catch (RuntimeIOException ex) {
-                throw new RuntimeIOException("Problem writing temporary file " + f.getAbsolutePath() +
+                throw new RuntimeIOException("Problem writing temporary file " + f.toUri() +
                         ".  Try setting TMP_DIR to a file system with lots of space.", ex);
             } finally {
                 if (os != null) {
@@ -251,8 +251,8 @@ public class SortingCollection<T> implements Iterable<T> {
      * Creates a new tmp file on one of the available temp filesystems, registers it for deletion
      * on JVM exit and then returns it.
      */
-    private File newTempFile() throws IOException {
-        return IOUtil.newTempFile("sortingcollection.", ".tmp", this.tmpDirs, TMP_SPACE_FREE);
+    private Path newTempFile() throws IOException {
+        return IOUtil.newTempPath("sortingcollection.", ".tmp", this.tmpDirs, TMP_SPACE_FREE);
     }
 
     /**
@@ -281,7 +281,7 @@ public class SortingCollection<T> implements Iterable<T> {
         this.iterationStarted = true;
         this.cleanedUp = true;
 
-        IOUtil.deleteFiles(this.files);
+        IOUtil.deletePaths(this.files);
     }
 
     /**
@@ -292,13 +292,16 @@ public class SortingCollection<T> implements Iterable<T> {
      * @param comparator Defines output sort order
      * @param maxRecordsInRAM how many records to accumulate in memory before spilling to disk
      * @param tmpDir Where to write files of records that will not fit in RAM
+     *
+     * @deprecated since 2017-09. Use {@link #newInstance(Class, Codec, Comparator, int, Path...)} instead
      */
+    @Deprecated
     public static <T> SortingCollection<T> newInstance(final Class<T> componentType,
                                                        final SortingCollection.Codec<T> codec,
                                                        final Comparator<T> comparator,
                                                        final int maxRecordsInRAM,
                                                        final File... tmpDir) {
-        return new SortingCollection<T>(componentType, codec, comparator, maxRecordsInRAM, tmpDir);
+        return new SortingCollection<T>(componentType, codec, comparator, maxRecordsInRAM, Arrays.stream(tmpDir).map(File::toPath).toArray(Path[]::new));
 
     }
 
@@ -310,7 +313,10 @@ public class SortingCollection<T> implements Iterable<T> {
      * @param comparator Defines output sort order
      * @param maxRecordsInRAM how many records to accumulate in memory before spilling to disk
      * @param tmpDirs Where to write files of records that will not fit in RAM
+     *
+     * @deprecated since 2017-09. Use {@link #newInstanceFromPaths(Class, Codec, Comparator, int, Collection)} instead
      */
+    @Deprecated
     public static <T> SortingCollection<T> newInstance(final Class<T> componentType,
                                                        final SortingCollection.Codec<T> codec,
                                                        final Comparator<T> comparator,
@@ -320,7 +326,7 @@ public class SortingCollection<T> implements Iterable<T> {
                                         codec,
                                         comparator,
                                         maxRecordsInRAM,
-                                        tmpDirs.toArray(new File[tmpDirs.size()]));
+                                        tmpDirs.stream().map(File::toPath).toArray(Path[]::new));
 
     }
 
@@ -338,8 +344,48 @@ public class SortingCollection<T> implements Iterable<T> {
                                                        final Comparator<T> comparator,
                                                        final int maxRecordsInRAM) {
 
-        final File tmpDir = new File(System.getProperty("java.io.tmpdir"));
+        final Path tmpDir = IOUtil.getDefaultTmpDirPath();
         return new SortingCollection<T>(componentType, codec, comparator, maxRecordsInRAM, tmpDir);
+    }
+
+    /**
+     * Syntactic sugar around the ctor, to save some typing of type parameters
+     *
+     * @param componentType Class of the record to be sorted.  Necessary because of Java generic lameness.
+     * @param codec For writing records to file and reading them back into RAM
+     * @param comparator Defines output sort order
+     * @param maxRecordsInRAM how many records to accumulate in memory before spilling to disk
+     * @param tmpDir Where to write files of records that will not fit in RAM
+     */
+    public static <T> SortingCollection<T> newInstance(final Class<T> componentType,
+            final SortingCollection.Codec<T> codec,
+            final Comparator<T> comparator,
+            final int maxRecordsInRAM,
+            final Path... tmpDir) {
+        return new SortingCollection<T>(componentType, codec, comparator, maxRecordsInRAM, tmpDir);
+
+    }
+
+    /**
+     * Syntactic sugar around the ctor, to save some typing of type parameters
+     *
+     * @param componentType Class of the record to be sorted.  Necessary because of Java generic lameness.
+     * @param codec For writing records to file and reading them back into RAM
+     * @param comparator Defines output sort order
+     * @param maxRecordsInRAM how many records to accumulate in memory before spilling to disk
+     * @param tmpDirs Where to write files of records that will not fit in RAM
+     */
+    public static <T> SortingCollection<T> newInstanceFromPaths(final Class<T> componentType,
+            final SortingCollection.Codec<T> codec,
+            final Comparator<T> comparator,
+            final int maxRecordsInRAM,
+            final Collection<Path> tmpDirs) {
+        return new SortingCollection<T>(componentType,
+                codec,
+                comparator,
+                maxRecordsInRAM,
+                tmpDirs.toArray(new Path[tmpDirs.size()]));
+
     }
 
     /**
@@ -403,7 +449,7 @@ public class SortingCollection<T> implements Iterable<T> {
         MergingIterator() {
             this.queue = new TreeSet<PeekFileRecordIterator>(new PeekFileRecordIteratorComparator());
             int n = 0;
-            for (final File f : SortingCollection.this.files) {
+            for (final Path f : SortingCollection.this.files) {
                 final FileRecordIterator it = new FileRecordIterator(f);
                 if (it.hasNext()) {
                     this.queue.add(new PeekFileRecordIterator(it, n++));
@@ -455,20 +501,20 @@ public class SortingCollection<T> implements Iterable<T> {
      * Read a file of records in format defined by the codec
      */
     class FileRecordIterator implements CloseableIterator<T> {
-        private final File file;
-        private final FileInputStream is;
+        private final Path file;
+        private final InputStream is;
         private final Codec<T> codec;
         private T currentRecord = null;
 
-        FileRecordIterator(final File file) {
+        FileRecordIterator(final Path file) {
             this.file = file;
             try {
-                this.is = new FileInputStream(file);
+                this.is = Files.newInputStream(file);
                 this.codec = SortingCollection.this.codec.clone();
                 this.codec.setInputStream(tempStreamFactory.wrapTempInputStream(this.is, Defaults.BUFFER_SIZE));
                 advance();
             }
-            catch (FileNotFoundException e) {
+            catch (IOException e) {
                 throw new RuntimeIOException(e);
             }
         }

src/main/java/htsjdk/samtools/util/SortingLongCollection.java
@@ -32,6 +32,8 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.Serializable;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
@@ -69,7 +71,7 @@ public class SortingLongCollection {
     /**
      * Where files of sorted values go.
      */
-    private final File[] tmpDir;
+    private final Path[] tmpDir;
 
     private final int maxValuesInRam;
     private int numValuesInRam = 0;
@@ -89,7 +91,7 @@ public class SortingLongCollection {
     /**
      * List of files in tmpDir containing sorted values
      */
-    private final List<File> files = new ArrayList<File>();
+    private final List<Path> files = new ArrayList<>();
 
     // for in-memory iteration
     private int iterationIndex = 0;
@@ -104,6 +106,16 @@ public class SortingLongCollection {
      * @param tmpDir         Where to write files of values that will not fit in RAM
      */
     public SortingLongCollection(final int maxValuesInRam, final File... tmpDir) {
+        this(maxValuesInRam, Arrays.stream(tmpDir).map(File::toPath).toArray(Path[]::new));
+    }
+
+    /**
+     * Prepare to accumulate values to be sorted
+     *
+     * @param maxValuesInRam how many values to accumulate before spilling to disk
+     * @param tmpDir         Where to write files of values that will not fit in RAM
+     */
+    public SortingLongCollection(final int maxValuesInRam, final Path... tmpDir) {
         if (maxValuesInRam <= 0) {
             throw new IllegalArgumentException("maxValuesInRam must be > 0");
         }
@@ -147,7 +159,7 @@ public class SortingLongCollection {
 
         this.priorityQueue = new PriorityQueue<PeekFileValueIterator>(files.size(),
                 new PeekFileValueIteratorComparator());
-        for (final File f : files) {
+        for (final Path f : files) {
             final FileValueIterator it = new FileValueIterator(f);
             if (it.hasNext()) {
                 this.priorityQueue.offer(new PeekFileValueIterator(it));
@@ -165,12 +177,12 @@ public class SortingLongCollection {
 
         try {
             Arrays.sort(this.ramValues, 0, this.numValuesInRam);
-            final File f = IOUtil.newTempFile("sortingcollection.", ".tmp", this.tmpDir, IOUtil.FIVE_GBS);
+            final Path f = IOUtil.newTempPath("sortingcollection.", ".tmp", this.tmpDir, IOUtil.FIVE_GBS);
             DataOutputStream os = null;
             try {
                 final long numBytes = this.numValuesInRam * SIZEOF;
-                os = new DataOutputStream(IOUtil.maybeBufferOutputStream(new FileOutputStream(f)));
-                f.deleteOnExit();
+                os = new DataOutputStream(IOUtil.maybeBufferOutputStream(Files.newOutputStream(f)));
+                IOUtil.deleteOnExit(f);
                 for (int i = 0; i < this.numValuesInRam; ++i) {
                     os.writeLong(ramValues[i]);
                 }
@@ -197,7 +209,7 @@ public class SortingLongCollection {
         this.cleanedUp = true;
         this.ramValues = null;
 
-        IOUtil.deleteFiles(this.files);
+        IOUtil.deletePaths(this.files);
     }
 
 
@@ -247,18 +259,18 @@ public class SortingLongCollection {
      * Read a file of longs
      */
     private static class FileValueIterator {
-        private final File file;
+        private final Path file;
         private final DataInputStream is;
         private long currentRecord = 0;
         private boolean isCurrentRecord = true;
 
-        FileValueIterator(final File file) {
+        FileValueIterator(final Path file) {
             this.file = file;
             try {
-                is = new DataInputStream(IOUtil.maybeBufferInputStream(new FileInputStream(file)));
+                is = new DataInputStream(IOUtil.maybeBufferInputStream(Files.newInputStream(file)));
                 next();
-            } catch (FileNotFoundException e) {
-                throw new RuntimeIOException(file.getAbsolutePath(), e);
+            } catch (IOException e) {
+                throw new RuntimeIOException(file.toUri().toString(), e);
             }
         }
 

commit d3d40c5024f259a7ad4e1cd7326c07c46000fe3a

fixing crash on shutdown hook #1029
fixing the crash on shutdown introduced in #910 by changin the command
that gets run on deleteOnExit
Files.delete() -> Files.deleteIfExists()
this will prevent the jvm from crashing on shutdown if the temporary
files get cleaned up before it closes

This still leaves open the possibilty of crash on shutdown if the
temporary file is unable to be removed for some other reason.  That's
subtly different than the File version of deleteOnExit, but it may be
useful to get these error messages in the unusual case of failing to
delete a file that exists.

src/main/java/htsjdk/samtools/util/IOUtil.java
@@ -360,16 +360,16 @@ public class IOUtil {
      *
      * @see #deleteOnExit(Path)
      */
-    protected static final class DeletePathThread extends Thread {
+    static final class DeletePathThread extends Thread {
 
         private final Path path;
 
-        protected DeletePathThread(Path path) {this.path = path;}
+        DeletePathThread(Path path) {this.path = path;}
 
         @Override
         public void run() {
             try {
-                Files.delete(path);
+                Files.deleteIfExists(path);
             } catch (IOException e) {
                 throw new RuntimeIOException(e);
             }