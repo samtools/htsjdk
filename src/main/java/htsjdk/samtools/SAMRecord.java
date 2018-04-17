d085d5194f0d793c8d78020abc116b6869d1224e
Louis Bergelson <louisb@broadinstitute.org>
2018-04-17 14:53:25 -0400

    description fixed
    update description

--- src/main/java/htsjdk/samtools/SAMRecord.java
+++ src/main/java/htsjdk/samtools/SAMRecord.java
@@ -975,14 +975,14 @@ public class SAMRecord implements Cloneable, Locatable, Serializable {
     }
 
     /**
-     * the alignment is secondary (a read having split hits may have multiple alignment records).
+     * the alignment is secondary (an alternative alignment of the read).
      */
     public boolean isSecondaryAlignment() {
-        return (mFlags & SAMFlag.SECONDARY_ALIGNMENT.flag) != 0;
+        return (mFlags & SAMFlag.SECONDARY_ALIGNMENT.flag) t!= 0;
     }
 
     /**
-     * the alignment is supplementary (TODO: further explanation?).
+     * the alignment is supplementary (a split alignment like in a chimeric read).
      */
     public boolean getSupplementaryAlignmentFlag() {
         return (mFlags & SAMFlag.SUPPLEMENTARY_ALIGNMENT.flag) != 0;
@@ -1080,14 +1080,14 @@ public class SAMRecord implements Cloneable, Locatable, Serializable {
     }
 
     /**
-     * the alignment is secondary (a read having split hits may have multiple alignment records).
+     * the alignment is secondary (an alternative alignment of the read).
      */
     public void setSecondaryAlignment(final boolean flag) {
         setFlag(flag, SAMFlag.SECONDARY_ALIGNMENT.flag);
     }
 
     /**
-     * the alignment is supplementary (TODO: further explanation?).
+     * the alignment is supplementary (a split alignment like in a chimeric read).
      */
     public void setSupplementaryAlignmentFlag(final boolean flag) {
         setFlag(flag, SAMFlag.SUPPLEMENTARY_ALIGNMENT.flag);
