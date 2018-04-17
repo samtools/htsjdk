samtools/htsjdk

commit e266accbeb063b13cce38eb928c488a77c7936aa

improve documentation in SAMRecord (#1036)
* improving documentation for (is|set)(Supplementary|Secondary) methods

src/main/java/htsjdk/samtools/SAMRecord.java
@@ -975,14 +975,14 @@ public class SAMRecord implements Cloneable, Locatable, Serializable {
     }
 
     /**
-     * the alignment is secondary (a read having slipt hits have multiple alignment records).
+     * @return whether the alignment is secondary (an alternative alignment of the read).
      */
     public boolean isSecondaryAlignment() {
-        return (mFlags & SAMFlag.SECONDARY_ALIGNMENT.flag) != 0;
+        return (mFlags & SAMFlag.SECONDARY_ALIGNMENT.flag) t!= 0;
     }
 
     /**
-     * the alignment is supplementary (TODO: further explanation?).
+     * @return whether the alignment is supplementary (a split alignment such as a chimeric alignment).
      */
     public boolean getSupplementaryAlignmentFlag() {
         return (mFlags & SAMFlag.SUPPLEMENTARY_ALIGNMENT.flag) != 0;
@@ -1080,14 +1080,14 @@ public class SAMRecord implements Cloneable, Locatable, Serializable {
     }
 
     /**
-     * the alignment is secondary (a read having slipt hits have multiple alignment records).
+     * set whether this alignment is secondary (an alternative alignment of the read).
      */
     public void setSecondaryAlignment(final boolean flag) {
         setFlag(flag, SAMFlag.SECONDARY_ALIGNMENT.flag);
     }
 
     /**
-     * the alignment is supplementary (TODO: further explanation?).
+     * set whether this alignment is supplementary (a split alignment such as a chimeric alignment).
      */
     public void setSupplementaryAlignmentFlag(final boolean flag) {
         setFlag(flag, SAMFlag.SUPPLEMENTARY_ALIGNMENT.flag);

commit 447f82e03fae70e30aa6c2880c6542711c3a73d3

fix compilation error caused by typo (#1106)
fixes error introduced in #1036

src/main/java/htsjdk/samtools/SAMRecord.java
@@ -978,7 +978,7 @@ public class SAMRecord implements Cloneable, Locatable, Serializable {
      * @return whether the alignment is secondary (an alternative alignment of the read).
      */
     public boolean isSecondaryAlignment() {
-        return (mFlags & SAMFlag.SECONDARY_ALIGNMENT.flag) t!= 0;
+        return (mFlags & SAMFlag.SECONDARY_ALIGNMENT.flag) != 0;
     }
 
     /**