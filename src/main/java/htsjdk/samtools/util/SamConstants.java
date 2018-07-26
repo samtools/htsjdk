b97392cec01611e0f5b73a2f2d1dd4f9bf4e37a5
Yossi Farjoun <farjoun@broadinstitute.org>
2018-07-26 17:11:25 -0400

    - adding some constants due to recent PR's to the spec.

---
+++ src/main/java/htsjdk/samtools/util/SamConstants.java
@@ -0,0 +1,11 @@
+package htsjdk.samtools.util;
+
+/**
+ * A set of constants defined in the sam-spec (https://github.com/samtools/hts-specs) that need
+ * to be referenced in code.
+ */
+public class SamConstants {
+
+    final static String BARCODE_SEQUENCE_DELIMITER = "-";
+    final static String BARCODE_QUALITY_DELIMITER = "~";
+}
