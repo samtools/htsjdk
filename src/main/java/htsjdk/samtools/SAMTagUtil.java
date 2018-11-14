db83cfff4d2cf1a5de9c598bfef9031f69067c90
Louis Bergelson <louisb@broadinstitute.org>
2018-11-14 15:40:52 -0500

    more changes to SAMTagUtil
    * reverting the change from constants to static variables, deprecating
    the constants instead

--- src/main/java/htsjdk/samtools/SAMTagUtil.java
+++ src/main/java/htsjdk/samtools/SAMTagUtil.java
@@ -27,9 +27,11 @@ import htsjdk.samtools.util.StringUtil;
 
 /**
  * Facility for converting between String and short representation of a SAM tag.  short representation
- * is used by SAM JDK internally and is much more efficient.  Callers are encouraged to obtain the short
+ * is used by Htsjdk internally and is much more efficient.  Callers are encouraged to obtain the short
  * value for a tag of interest once, and then use the SAMRecord attribute API that takes shorts rather than
  * Strings.
+ *
+ * Tags that are defined by the SAM spec are included in the enum {@link SAMTag} along with their precomputed short tag.
  *
  * @author alecw@broadinstitute.org
  */
@@ -41,82 +43,141 @@ public final class SAMTagUtil {
     @Deprecated
     public SAMTagUtil(){}
 
-    // Standard tags pre-computed for convenience
-    public static final short AM = SAMTag.AM.getBinaryTag();
-    public static final short AS = SAMTag.AS.getBinaryTag();
-    public static final short BC = SAMTag.BC.getBinaryTag();
-    public static final short BQ = SAMTag.BQ.getBinaryTag();
-    public static final short BZ = SAMTag.BZ.getBinaryTag();
-    public static final short CB = SAMTag.CB.getBinaryTag();
-    public static final short CC = SAMTag.CC.getBinaryTag();
-    public static final short CG = SAMTag.CG.getBinaryTag();
-    public static final short CM = SAMTag.CM.getBinaryTag();
-    public static final short CO = SAMTag.CO.getBinaryTag();
-    public static final short CP = SAMTag.CP.getBinaryTag();
-    public static final short CQ = SAMTag.CQ.getBinaryTag();
-    public static final short CR = SAMTag.CR.getBinaryTag();
-    public static final short CS = SAMTag.CS.getBinaryTag();
-    public static final short CT = SAMTag.CT.getBinaryTag();
-    public static final short CY = SAMTag.CY.getBinaryTag();
-    public static final short E2 = SAMTag.E2.getBinaryTag();
-    public static final short FI = SAMTag.FI.getBinaryTag();
-    public static final short FS = SAMTag.FS.getBinaryTag();
-    public static final short FT = SAMTag.FT.getBinaryTag();
-    public static final short FZ = SAMTag.FZ.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short AM = SAMTag.AM.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short AS = SAMTag.AS.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short BC = SAMTag.BC.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short BQ = SAMTag.BQ.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short BZ = SAMTag.BZ.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short CB = SAMTag.CB.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short CC = SAMTag.CC.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short CG = SAMTag.CG.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short CM = SAMTag.CM.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short CO = SAMTag.CO.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short CP = SAMTag.CP.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short CQ = SAMTag.CQ.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short CR = SAMTag.CR.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short CS = SAMTag.CS.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short CT = SAMTag.CT.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short CY = SAMTag.CY.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short E2 = SAMTag.E2.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short FI = SAMTag.FI.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short FS = SAMTag.FS.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short FT = SAMTag.FT.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short FZ = SAMTag.FZ.getBinaryTag();
+
     /** @deprecated reserved tag for backwards compatibility only */
-    @Deprecated
-    public static final short GC = SAMTag.GC.getBinaryTag();
+    @Deprecated public final short GC = SAMTag.GC.getBinaryTag();
     /** @deprecated reserved tag for backwards compatibility only */
-    @Deprecated
-    public static final short GS = SAMTag.GS.getBinaryTag();
+    @Deprecated public final short GS = SAMTag.GS.getBinaryTag();
     /** @deprecated reserved tag for backwards compatibility only */
-    @Deprecated
-    public static final short GQ = SAMTag.GQ.getBinaryTag();
-    public static final short LB = SAMTag.LB.getBinaryTag();
-    public static final short H0 = SAMTag.H0.getBinaryTag();
-    public static final short H1 = SAMTag.H1.getBinaryTag();
-    public static final short H2 = SAMTag.H2.getBinaryTag();
-    public static final short HI = SAMTag.HI.getBinaryTag();
-    public static final short IH = SAMTag.IH.getBinaryTag();
-    public static final short MC = SAMTag.MC.getBinaryTag();
+    @Deprecated public final short GQ = SAMTag.GQ.getBinaryTag();
+
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short LB = SAMTag.LB.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short H0 = SAMTag.H0.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short H1 = SAMTag.H1.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short H2 = SAMTag.H2.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short HI = SAMTag.HI.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short IH = SAMTag.IH.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short MC = SAMTag.MC.getBinaryTag();
+
     /** @deprecated reserved tag for backwards compatibility only */
-    @Deprecated
-    public static final short MF = SAMTag.MF.getBinaryTag();
-    public static final short MI = SAMTag.MI.getBinaryTag();
-    public static final short MD = SAMTag.MD.getBinaryTag();
-    public static final short MQ = SAMTag.MQ.getBinaryTag();
-    public static final short NH = SAMTag.NH.getBinaryTag();
-    public static final short NM = SAMTag.NM.getBinaryTag();
-    public static final short OQ = SAMTag.OQ.getBinaryTag();
-    public static final short OP = SAMTag.OP.getBinaryTag();
-    public static final short OC = SAMTag.OC.getBinaryTag();
-    public static final short OF = SAMTag.OF.getBinaryTag();
-    public static final short OR = SAMTag.OR.getBinaryTag();
-    public static final short OX = SAMTag.OX.getBinaryTag();
-    public static final short PG = SAMTag.PG.getBinaryTag();
-    public static final short PQ = SAMTag.PQ.getBinaryTag();
-    public static final short PT = SAMTag.PT.getBinaryTag();
-    public static final short PU = SAMTag.PU.getBinaryTag();
-    public static final short QT = SAMTag.QT.getBinaryTag();
-    public static final short Q2 = SAMTag.Q2.getBinaryTag();
-    public static final short QX = SAMTag.QX.getBinaryTag();
-    public static final short R2 = SAMTag.R2.getBinaryTag();
-    public static final short RG = SAMTag.RG.getBinaryTag();
+    @Deprecated public final short MF = SAMTag.MF.getBinaryTag();
+
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short MI = SAMTag.MI.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short MD = SAMTag.MD.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short MQ = SAMTag.MQ.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short NH = SAMTag.NH.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short NM = SAMTag.NM.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short OQ = SAMTag.OQ.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short OP = SAMTag.OP.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short OC = SAMTag.OC.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short OF = SAMTag.OF.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short OR = SAMTag.OR.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short OX = SAMTag.OX.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short PG = SAMTag.PG.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short PQ = SAMTag.PQ.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short PT = SAMTag.PT.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short PU = SAMTag.PU.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short QT = SAMTag.QT.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short Q2 = SAMTag.Q2.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short QX = SAMTag.QX.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short R2 = SAMTag.R2.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short RG = SAMTag.RG.getBinaryTag();
+
     /** @deprecated use BC instead */
     @Deprecated
-    public static final short RT = SAMTag.RT.getBinaryTag();
-    public static final short RX = SAMTag.RX.getBinaryTag();
+    public final short RT = SAMTag.RT.getBinaryTag();
+
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short RX = SAMTag.RX.getBinaryTag();
+    /** @deprecated reserved tag for backwards compatibility only */
+
+    @Deprecated
+    public final short S2 = SAMTag.S2.getBinaryTag();
+
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short SA = SAMTag.SA.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short SM = SAMTag.SM.getBinaryTag();
+
     /** @deprecated reserved tag for backwards compatibility only */
     @Deprecated
-    public static final short S2 = SAMTag.S2.getBinaryTag();
-    public static final short SA = SAMTag.SA.getBinaryTag();
-    public static final short SM = SAMTag.SM.getBinaryTag();
-    /** @deprecated reserved tag for backwards compatibility only */
-    @Deprecated
-    public static final short SQ = SAMTag.SQ.getBinaryTag();
-    public static final short TC = SAMTag.TC.getBinaryTag();
-    public static final short U2 = SAMTag.U2.getBinaryTag();
-    public static final short UQ = SAMTag.UQ.getBinaryTag();
+    public final short SQ = SAMTag.SQ.getBinaryTag();
+
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short TC = SAMTag.TC.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short U2 = SAMTag.U2.getBinaryTag();
+    /** @deprecated use {@link SAMTag#getBinaryTag()} instead. */
+    @Deprecated public final short UQ = SAMTag.UQ.getBinaryTag();
 
     @Deprecated
     private static final SAMTagUtil SINGLETON = new SAMTagUtil();
@@ -128,7 +189,7 @@ public final class SAMTagUtil {
     /**
      * Despite the fact that this class has state, it should be thread-safe because the cache
      * gets filled with the same values by any thread.
-     * @deprecated All methods on this class have been made static, use them directly
+     * @deprecated All methods on this class have been made static, use them directly. Instead of using the constants in this class use {@link SAMTag#getBinaryTag()}.
      */
     @Deprecated
     public static SAMTagUtil getSingleton() {
