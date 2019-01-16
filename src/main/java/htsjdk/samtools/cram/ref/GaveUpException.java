b5d0959c6bfe9b7a83805c07fd4e034bcab61eb9
Joel Thibault <thibault@broadinstitute.org>
2019-01-16 11:13:29 -0500

    Immutable GaveUpException

--- src/main/java/htsjdk/samtools/cram/ref/GaveUpException.java
+++ src/main/java/htsjdk/samtools/cram/ref/GaveUpException.java
@@ -2,17 +2,13 @@ package htsjdk.samtools.cram.ref;
 
 public class GaveUpException extends RuntimeException {
     private static final long serialVersionUID = -8997576068346912410L;
-    private String md5;
+    private final String md5;
 
-    public GaveUpException(final String md5) {
-        this.setMd5(md5);
+    GaveUpException(final String md5) {
+        this.md5 = md5;
     }
 
     public String getMd5() {
         return md5;
-    }
-
-    public void setMd5(final String md5) {
-        this.md5 = md5;
     }
 }
