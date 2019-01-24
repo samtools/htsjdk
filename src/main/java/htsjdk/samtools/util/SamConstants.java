421c421b82c9b2ba0e6c8c3412d8cb8b0f268550
Louis Bergelson <louisb@broadinstitute.org>
2019-01-24 12:15:17 -0500

    Update src/main/java/htsjdk/samtools/util/SamConstants.java
    Co-Authored-By: yfarjoun <farjoun@broadinstitute.org>

--- src/main/java/htsjdk/samtools/util/SamConstants.java
+++ src/main/java/htsjdk/samtools/util/SamConstants.java
@@ -4,7 +4,7 @@ package htsjdk.samtools.util;
  * A set of constants defined in the sam-spec (https://github.com/samtools/hts-specs) that need
  * to be referenced in code.
  */
-public class SamConstants {
+ public final class SamConstants {
 
     final static String BARCODE_SEQUENCE_DELIMITER = "-";
     final static String BARCODE_QUALITY_DELIMITER = " ";
