56a3837db57b203eeab517d59a40eedbf8ee06e1
Yossi Farjoun <farjoun@broadinstitute.org>
2019-02-02 14:38:28 -0500

    - cleanup

--- src/test/java/htsjdk/samtools/cram/ref/EnaRefServiceTest.java
+++ src/test/java/htsjdk/samtools/cram/ref/EnaRefServiceTest.java
@@ -18,7 +18,7 @@ public class EnaRefServiceTest extends HtsjdkTest {
 
     //calling this an FTP test so that it could fail in travis
     @Test(dataProvider = "testEnaRefServiceData")
-    public void testEnaRefService(final String md5) throws IOException, GaveUpException {
+    public void testEnaRefServiceData(final String md5) throws IOException, GaveUpException {
         Assert.assertNotNull(new EnaRefService().getSequence(md5));
     }
 }
