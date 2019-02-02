aee3b778c848ee055c19bc0dbaaf81ec7896501c
Yossi Farjoun <farjoun@broadinstitute.org>
2019-02-02 14:07:28 -0500

    - ENA tests also fail internitenetly, so making them part of the ftp group

--- src/test/java/htsjdk/samtools/cram/ref/EnaRefServiceTest.java
+++ src/test/java/htsjdk/samtools/cram/ref/EnaRefServiceTest.java
@@ -9,14 +9,15 @@ import java.io.IOException;
 
 public class EnaRefServiceTest extends HtsjdkTest {
 
-    @DataProvider(name="testEnaRefServiceData")
+    @DataProvider()
     public Object[][] testEnaRefServiceData(){
         return new Object[][]{{"57151e6196306db5d9f33133572a5482"},
                 {"0000088cbcebe818eb431d58c908c698"}};
     }
 
-    @Test(dataProvider = "testEnaRefServiceData")
-    public void testEnaRefServiceData(final String md5) throws IOException, GaveUpException {
+    //calling this an FTP test so that it could fail in travis
+    @Test(dataProvider = "testEnaRefServiceData", groups="ftp")
+    public void testEnaRefService(final String md5) throws IOException, GaveUpException {
         Assert.assertNotNull(new EnaRefService().getSequence(md5));
     }
 }
