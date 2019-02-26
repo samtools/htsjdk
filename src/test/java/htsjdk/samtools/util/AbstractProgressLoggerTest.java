ba3ec0f7a39b4f58f11431a566c63c6248b9d637
Yossi Farjoun <farjoun@broadinstitute.org>
2019-02-25 22:43:18 -0500

    -converted test to dataprovider

--- src/test/java/htsjdk/samtools/util/AbstractProgressLoggerTest.java
+++ src/test/java/htsjdk/samtools/util/AbstractProgressLoggerTest.java
@@ -2,16 +2,24 @@ package htsjdk.samtools.util;
 
 import htsjdk.HtsjdkTest;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class AbstractProgressLoggerTest extends HtsjdkTest {
 
-    @Test
-    public void testPad() {
-        Assert.assertEquals(AbstractProgressLogger.pad("hello",10),"     hello");
-        Assert.assertEquals(AbstractProgressLogger.pad("hello",6)," hello");
-        Assert.assertEquals(AbstractProgressLogger.pad("hello",5),"hello");
-        Assert.assertEquals(AbstractProgressLogger.pad("hello",4),"hello");
-        Assert.assertEquals(AbstractProgressLogger.pad("hello",-1),"hello");
+    @DataProvider
+    Object[][] testPadData() {
+        return new Object[][]{
+                {"hello", 10, "     hello"},
+                {"hello", 6, " hello"},
+                {"hello", 5, "hello"},
+                {"hello", 4, "hello"},
+                {"hello", -1, "hello"}
+        };
+    }
+
+    @Test(dataProvider = "testPadData")
+    public void testPad(final String in, final int length, final String expected) {
+        Assert.assertEquals(AbstractProgressLogger.pad(in, length), expected);
     }
 }
