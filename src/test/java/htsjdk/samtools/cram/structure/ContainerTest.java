1509dcc262689f904e30afeaa561ac7e30a4b8ec
jmthibault79 <thibault@broadinstitute.org>
2019-03-02 09:00:20 -0500

    Consolidate common code into CRAMStructureTestUtil: (#1312)
    - ContainerTest, ContainerFactoryTest, ContainerParserTest, SliceTests, and CRAMBAIIndexerTest

--- src/test/java/htsjdk/samtools/cram/structure/ContainerTest.java
+++ src/test/java/htsjdk/samtools/cram/structure/ContainerTest.java
@@ -3,102 +3,83 @@ package htsjdk.samtools.cram.structure;
 import htsjdk.HtsjdkTest;
 import htsjdk.samtools.cram.CRAMException;
 import htsjdk.samtools.cram.ref.ReferenceContext;
-import htsjdk.samtools.cram.ref.ReferenceContextType;
-import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class ContainerTest extends HtsjdkTest {
-    @Test
-    public static void singleRefSliceStateTest() {
-        final Slice slice = new Slice(new ReferenceContext(5));
-        slice.alignmentStart = 10;
-        slice.alignmentSpan = 15;
+    @DataProvider(name = "containerStateTestCases")
+    private Object[][] containerStateTestCases() {
+        final ReferenceContext mappedReferenceContext = new ReferenceContext(5); // arbitrary
 
-        final Container container = Container.initializeFromSlices(Arrays.asList(slice));
+        final int slice1AlignmentStart = 10;
+        final int slice1AlignmentSpan = 15;
+        final Slice mappedSlice1 = new Slice(mappedReferenceContext);
+        mappedSlice1.alignmentStart = slice1AlignmentStart;
+        mappedSlice1.alignmentSpan = slice1AlignmentSpan;
 
-        Assert.assertTrue(container.getReferenceContext().isMappedSingleRef());
-        Assert.assertEquals(container.getReferenceContext().getType(), ReferenceContextType.SINGLE_REFERENCE_TYPE);
-        Assert.assertEquals(container.getReferenceContext(), slice.getReferenceContext());
-        Assert.assertEquals(container.alignmentStart, slice.alignmentStart);
-        Assert.assertEquals(container.alignmentSpan, slice.alignmentSpan);
+        final int slice2AlignmentStart = 20;
+        final int slice2AlignmentSpan = 20;
+        final Slice mappedSlice2 = new Slice(mappedReferenceContext);
+        mappedSlice2.alignmentStart = slice2AlignmentStart;
+        mappedSlice2.alignmentSpan = slice2AlignmentSpan;
+        final int expectedSpan = slice2AlignmentStart + slice2AlignmentSpan - slice1AlignmentStart;
+
+        return new Object[][] {
+                {
+                    Arrays.asList(mappedSlice1), mappedReferenceContext, slice1AlignmentStart, slice1AlignmentSpan
+                },
+                {
+                    Arrays.asList(mappedSlice1, mappedSlice2), mappedReferenceContext, slice1AlignmentStart, expectedSpan
+                },
+                {
+                    Arrays.asList(new Slice(ReferenceContext.MULTIPLE_REFERENCE_CONTEXT),
+                            new Slice(ReferenceContext.MULTIPLE_REFERENCE_CONTEXT)),
+                    ReferenceContext.MULTIPLE_REFERENCE_CONTEXT, Slice.NO_ALIGNMENT_START, Slice.NO_ALIGNMENT_SPAN
+                },
+                {
+                    Arrays.asList(new Slice(ReferenceContext.UNMAPPED_UNPLACED_CONTEXT),
+                            new Slice(ReferenceContext.UNMAPPED_UNPLACED_CONTEXT)),
+                    ReferenceContext.UNMAPPED_UNPLACED_CONTEXT, Slice.NO_ALIGNMENT_START, Slice.NO_ALIGNMENT_SPAN
+                },
+        };
     }
 
-    @Test
-    public static void singleRefSliceMultipleStateTest() {
-        final Slice slice1 = new Slice(new ReferenceContext(5));
-        slice1.alignmentStart = 10;
-        slice1.alignmentSpan = 15;
-
-        final Slice slice2 = new Slice(slice1.getReferenceContext());
-        slice2.alignmentStart = 20;
-        slice2.alignmentSpan = 20;
-
-        final Container container = Container.initializeFromSlices(Arrays.asList(slice1, slice2));
-
-        Assert.assertTrue(container.getReferenceContext().isMappedSingleRef());
-        Assert.assertEquals(container.getReferenceContext().getType(), ReferenceContextType.SINGLE_REFERENCE_TYPE);
-        Assert.assertEquals(container.getReferenceContext(), slice1.getReferenceContext());
-        Assert.assertEquals(container.alignmentStart, slice1.alignmentStart);
-        Assert.assertEquals(container.alignmentSpan, slice2.alignmentStart + slice2.alignmentSpan - slice1.alignmentStart);      // 20 + 20 - 10
+    @Test(dataProvider = "containerStateTestCases")
+    public void initializeFromSlicesTest(final List<Slice> slices,
+                                         final ReferenceContext expectedReferenceContext,
+                                         final int expectedAlignmentStart,
+                                         final int expectedAlignmentSpan) {
+        final Container container = Container.initializeFromSlices(slices);
+        CRAMStructureTestUtil.assertContainerState(container, expectedReferenceContext, expectedAlignmentStart, expectedAlignmentSpan);
     }
 
-    @Test
-    public static void multiRefSliceStateTest() {
-        final Slice slice1 = new Slice(ReferenceContext.MULTIPLE_REFERENCE_CONTEXT);
-        final Slice slice2 = new Slice(ReferenceContext.MULTIPLE_REFERENCE_CONTEXT);
-
-        final Container container = Container.initializeFromSlices(Arrays.asList(slice1, slice2));
-
-        Assert.assertTrue(container.getReferenceContext().isMultiRef());
-        Assert.assertEquals(container.getReferenceContext().getType(), ReferenceContextType.MULTIPLE_REFERENCE_TYPE);
-        Assert.assertEquals(container.alignmentStart, Slice.NO_ALIGNMENT_START);
-        Assert.assertEquals(container.alignmentSpan, Slice.NO_ALIGNMENT_SPAN);
+    @DataProvider(name = "illegalCombinationTestCases")
+    private Object[][] illegalCombinationTestCases() {
+        return new Object[][] {
+                {
+                        new Slice(new ReferenceContext(0)),
+                        new Slice(new ReferenceContext(1))
+                },
+                {
+                        new Slice(new ReferenceContext(0)),
+                        new Slice(ReferenceContext.UNMAPPED_UNPLACED_CONTEXT)
+                },
+                {
+                        new Slice(new ReferenceContext(0)),
+                        new Slice(ReferenceContext.MULTIPLE_REFERENCE_CONTEXT)
+                },
+                {
+                        new Slice(ReferenceContext.UNMAPPED_UNPLACED_CONTEXT),
+                        new Slice(ReferenceContext.MULTIPLE_REFERENCE_CONTEXT)
+                },
+        };
     }
 
-    @Test
-    public static void unmappedSliceStateTest() {
-        final Slice slice1 = new Slice(ReferenceContext.UNMAPPED_UNPLACED_CONTEXT);
-        final Slice slice2 = new Slice(ReferenceContext.UNMAPPED_UNPLACED_CONTEXT);
-
-        final Container container = Container.initializeFromSlices(Arrays.asList(slice1, slice2));
-
-        Assert.assertTrue(container.getReferenceContext().isUnmappedUnplaced());
-        Assert.assertEquals(container.getReferenceContext().getType(), ReferenceContextType.UNMAPPED_UNPLACED_TYPE);
-        Assert.assertEquals(container.alignmentStart, Slice.NO_ALIGNMENT_START);
-        Assert.assertEquals(container.alignmentSpan, Slice.NO_ALIGNMENT_SPAN);
-    }
-
-    @Test(expectedExceptions = CRAMException.class)
-    public static void differentReferencesStateTest() {
-        final Slice one = new Slice(new ReferenceContext(5));
-        final Slice another = new Slice(new ReferenceContext(2));
-
+    @Test(dataProvider = "illegalCombinationTestCases", expectedExceptions = CRAMException.class)
+    public static void illegalCombinationsStateTest(final Slice one, final Slice another) {
         Container.initializeFromSlices(Arrays.asList(one, another));
-    }
-
-    @Test(expectedExceptions = CRAMException.class)
-    public static void singleAndUnmappedStateTest() {
-        final Slice single = new Slice(new ReferenceContext(5));
-        final Slice unmapped = new Slice(ReferenceContext.UNMAPPED_UNPLACED_CONTEXT);
-
-        Container.initializeFromSlices(Arrays.asList(single, unmapped));
-    }
-
-    @Test(expectedExceptions = CRAMException.class)
-    public static void multiAndSingleStateTest() {
-        final Slice multi = new Slice(ReferenceContext.MULTIPLE_REFERENCE_CONTEXT);
-        final Slice single = new Slice(new ReferenceContext(5));
-
-        Container.initializeFromSlices(Arrays.asList(multi, single));
-    }
-
-    @Test(expectedExceptions = CRAMException.class)
-    public static void multiAndUnmappedStateTest() {
-        final Slice multi = new Slice(ReferenceContext.MULTIPLE_REFERENCE_CONTEXT);
-        final Slice unmapped = new Slice(ReferenceContext.UNMAPPED_UNPLACED_CONTEXT);
-
-        Container.initializeFromSlices(Arrays.asList(multi, unmapped));
     }
 }
