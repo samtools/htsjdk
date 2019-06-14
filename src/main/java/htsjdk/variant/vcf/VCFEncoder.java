21f511a7db91bca267d7dde0c47c86b938905814
Louis Bergelson <louisb@broadinstitute.org>
2019-06-14 12:28:46 -0400

    Output missing vcf fields as a single .
    * Previously vcf genotype fields with a specific number of elements were output as a comma seperated list of dots
    * This is valid, but unecessarily long and doesn't provide any additional information.
    * Now only a single dot will be emitted.
    * ex:  .,.,. -> .

--- src/main/java/htsjdk/variant/vcf/VCFEncoder.java
+++ src/main/java/htsjdk/variant/vcf/VCFEncoder.java
@@ -338,23 +338,6 @@ public class VCFEncoder {
                             }
                         } else {
                             Object val = g.hasExtendedAttribute(field) ? g.getExtendedAttribute(field) : VCFConstants.MISSING_VALUE_v4;
-
-                            final VCFFormatHeaderLine metaData = this.header.getFormatHeaderLine(field);
-                            if (metaData != null) {
-                                final int numInFormatField = metaData.getCount(vc);
-                                if (numInFormatField > 1 && val.equals(VCFConstants.MISSING_VALUE_v4)) {
-                                    // If we have a missing field but multiple values are expected, we need to construct a new string with all fields.
-                                    // For example, if Number=2, the string has to be ".,."
-                                    final StringBuilder sb = new StringBuilder(VCFConstants.MISSING_VALUE_v4);
-                                    for (int i = 1; i < numInFormatField; i++) {
-                                        sb.append(',');
-                                        sb.append(VCFConstants.MISSING_VALUE_v4);
-                                    }
-                                    val = sb.toString();
-                                }
-                            }
-
-                            // assume that if key is absent, then the given string encoding suffices
                             outputValue = formatVCFField(val);
                         }
                     }
