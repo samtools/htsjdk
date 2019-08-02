samtools/htsjdk

commit 1d4a3161df70992090b488c8485f2ed87daf14a0

make VariantContextBuilder safer (#1344)
Adds tests and fixes unsafe building patterns that were enabled by VariantContextBuilder.

This required a small change in the API around adding a genotype context. Now when .genotypes() is called with a GenotypeContext object the builder will call immutable() on that object, to make sure that the user doesn't change the GC prior to the creation of the VariantContext...If folks really need to reuse the GenotypeContext objects across multiple build calls, we could add a .genotypesKeepMutable() method with a scary javaDoc note.....

Another change in the API (somewhat) is that validation was added for the filters as per the hts-spec.

fixes #1365

src/main/java/htsjdk/variant/variantcontext/VariantContext.java
@@ -32,7 +32,17 @@ import htsjdk.variant.utils.GeneralUtils;
 import htsjdk.variant.vcf.*;
 
 import java.io.Serializable;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 /**
@@ -215,25 +225,25 @@ import java.util.stream.Collectors;
 public class VariantContext implements Feature, Serializable {
     public static final long serialVersionUID = 1L;
 
-    private final static boolean WARN_ABOUT_BAD_END = true;
-    private final static int MAX_ALLELE_SIZE_FOR_NON_SV = 150;
+    private static final boolean WARN_ABOUT_BAD_END = true;
+    private static final int MAX_ALLELE_SIZE_FOR_NON_SV = 150;
     private boolean fullyDecoded = false;
     protected CommonInfo commonInfo = null;
-    public final static double NO_LOG10_PERROR = CommonInfo.NO_LOG10_PERROR;
+    public static final double NO_LOG10_PERROR = CommonInfo.NO_LOG10_PERROR;
 
-    public final static Set<String> PASSES_FILTERS = Collections.unmodifiableSet(new LinkedHashSet<String>());
+    public static final Set<String> PASSES_FILTERS = Collections.emptySet();
 
     /** The location of this VariantContext */
-    final protected String contig;
-    final protected long start;
-    final protected long stop;
+    protected final String contig;
+    protected final long start;
+    protected final long stop;
     private final String ID;
 
     /** The type (cached for performance reasons) of this context */
     protected Type type = null;
 
     /** A set of the alleles segregating in this context */
-    final protected List<Allele> alleles;
+    protected final List<Allele> alleles;
 
     /** A mapping from sampleName -&gt; genotype objects for all genotypes associated with this context */
     protected GenotypesContext genotypes = null;
@@ -241,7 +251,7 @@ public class VariantContext implements Feature, Serializable {
     /** Counts for each of the possible Genotype types in this context */
     protected int[] genotypeCounts = null;
 
-    public final static GenotypesContext NO_GENOTYPES = GenotypesContext.NO_GENOTYPES;
+    public static final GenotypesContext NO_GENOTYPES = GenotypesContext.NO_GENOTYPES;
 
     // a fast cached access point to the ref / alt alleles for biallelic case
     private Allele REF = null;
@@ -253,9 +263,10 @@ public class VariantContext implements Feature, Serializable {
     private Boolean monomorphic = null;
 
     /*
-* Determine which genotype fields are in use in the genotypes in VC
-* @return an ordered list of genotype fields in use in VC.  If vc has genotypes this will always include GT first
-*/
+     * Determine which genotype fields are in use in the genotypes in VC
+     * @return an ordered list of genotype fields in use in VC.  If vc has genotypes this will always include GT first
+     */
+
     public List<String> calcVCFGenotypeKeys(final VCFHeader header) {
         final Set<String> keys = new HashSet<>();
 
@@ -306,12 +317,91 @@ public class VariantContext implements Feature, Serializable {
     //
     // ---------------------------------------------------------------------------------------------------------
 
+    //no controls and white-spaces characters, no semicolon.
+    public static final Pattern VALID_FILTER = Pattern.compile("^[!-:<-~]+$");
+
     public enum Validation {
-        ALLELES,
-        GENOTYPES
+        ALLELES() {
+            void validate(VariantContext variantContext) {
+                validateAlleles(variantContext);
+            }
+        },
+        GENOTYPES() {
+            void validate(VariantContext variantContext) {
+                validateGenotypes(variantContext);
+            }
+        },
+        FILTERS {
+            void validate(VariantContext variantContext) {
+                validateFilters(variantContext);
+            }
+        };
+
+        abstract void validate(VariantContext variantContext);
+
+
+        private static void validateAlleles(final VariantContext vc) {
+
+            boolean alreadySeenRef = false;
+
+            for (final Allele allele : vc.alleles) {
+                // make sure there's only one reference allele
+                if (allele.isReference()) {
+                    if (alreadySeenRef) {
+                        throw new IllegalArgumentException("BUG: Received two reference tagged alleles in VariantContext " + vc.alleles + " vc=" + vc);
+                    }
+                    alreadySeenRef = true;
+                }
+
+                if (allele.isNoCall()) {
+                    throw new IllegalArgumentException("BUG: Cannot add a no call allele to a variant context " + vc.alleles + " vc=" + vc);
+                }
+            }
+
+            // make sure there's one reference allele
+            if (!alreadySeenRef) {
+                throw new IllegalArgumentException("No reference allele found in VariantContext");
+            }
+        }
+
+        private static void validateGenotypes(final VariantContext variantContext) {
+
+            final ArrayList<Genotype> genotypes = variantContext.genotypes.getGenotypes();
+
+            if (genotypes == null) {
+                throw new IllegalStateException("Genotypes is null");
+            }
+
+            for (int i = 0; i < genotypes.size(); i++) {
+                final Genotype genotype = genotypes.get(i);
+                if (genotype.isAvailable()) {
+                    final List<Allele> alleles = genotype.getAlleles();
+                    for (int j = 0, size = alleles.size(); j < size; j++) {
+                        final Allele gAllele = alleles.get(j);
+                        if (!variantContext.hasAllele(gAllele) && gAllele.isCalled()) {
+                            throw new IllegalStateException("Allele in genotype " + gAllele + " not in the variant context " + alleles);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void validateFilters(final VariantContext variantContext) {
+            final Set<String> filters = variantContext.getFilters();
+            if (filters == null) {
+                return;
+            }
+
+            for (String filter : filters) {
+                if (!VALID_FILTER.matcher(filter).matches()) {
+                    throw new IllegalStateException("Filter '" + filter +
+                            "' contains an illegal character. It must conform to the regex ;'" + VALID_FILTER);
+                }
+            }
+        }
     }
 
-    private final static EnumSet<Validation> NO_VALIDATION = EnumSet.noneOf(Validation.class);
+    private static final EnumSet<Validation> NO_VALIDATION = EnumSet.noneOf(Validation.class);
 
     // ---------------------------------------------------------------------------------------------------------
     //
@@ -1198,7 +1288,7 @@ public class VariantContext implements Feature, Serializable {
 
         // maintain a list of non-symbolic alleles expected in the REF and ALT fields of the record
         // (we exclude symbolic alleles because it's commonly expected that they don't show up in the genotypes, e.g. with GATK gVCFs)
-        final List<Allele> reportedAlleles = new ArrayList<Allele>();
+        final List<Allele> reportedAlleles = new ArrayList<>();
         for ( final Allele allele : getAlleles() ) {
             if ( !allele.isSymbolic() )
                 reportedAlleles.add(allele);
@@ -1286,17 +1376,9 @@ public class VariantContext implements Feature, Serializable {
     //
     // ---------------------------------------------------------------------------------------------------------
 
-    private boolean validate(final EnumSet<Validation> validationToPerform) {
+    private void validate(final EnumSet<Validation> validationsToPerform) {
         validateStop();
-        for (final Validation val : validationToPerform ) {
-            switch (val) {
-                case ALLELES: validateAlleles(); break;
-                case GENOTYPES: validateGenotypes(); break;
-                default: throw new IllegalArgumentException("Unexpected validation mode " + val);
-            }
-        }
-
-        return true;
+        validationsToPerform.forEach(v->v.validate(this));
     }
 
     /**
@@ -1312,52 +1394,14 @@ public class VariantContext implements Feature, Serializable {
                         + " but this VariantContext contains an END key with value " + end;
                 if ( GeneralUtils.DEBUG_MODE_ENABLED && WARN_ABOUT_BAD_END ) {
                     System.err.println(message);
-                }
-                else {
+                } else {
                     throw new TribbleException(message);
                 }
             }
         } else {
             final long length = (stop - start) + 1;
-            if ( ! hasSymbolicAlleles() && length != getReference().length() ) {
+            if (!hasSymbolicAlleles() && length != getReference().length()) {
                 throw new IllegalStateException("BUG: GenomeLoc " + contig + ":" + start + "-" + stop + " has a size == " + length + " but the variation reference allele has length " + getReference().length() + " this = " + this);
-            }
-        }
-    }
-
-    private void validateAlleles() {
-
-        boolean alreadySeenRef = false;
-
-        for ( final Allele allele : alleles ) {
-            // make sure there's only one reference allele
-            if ( allele.isReference() ) {
-                if ( alreadySeenRef ) throw new IllegalArgumentException("BUG: Received two reference tagged alleles in VariantContext " + alleles + " this=" + this);
-                alreadySeenRef = true;
-            }
-
-            if ( allele.isNoCall() ) {
-                throw new IllegalArgumentException("BUG: Cannot add a no call allele to a variant context " + alleles + " this=" + this);
-            }
-        }
-
-        // make sure there's one reference allele
-        if ( ! alreadySeenRef )
-            throw new IllegalArgumentException("No reference allele found in VariantContext");
-    }
-
-    private void validateGenotypes() {
-        if ( this.genotypes == null ) throw new IllegalStateException("Genotypes is null");
-
-        for ( int i = 0; i < genotypes.size(); i++ ) {
-            Genotype genotype = genotypes.get(i);
-            if ( genotype.isAvailable() ) {
-                final List<Allele> alleles = genotype.getAlleles();
-                for ( int j = 0, size = alleles.size(); j < size; j++ ) {
-                    final Allele gAllele = alleles.get(j);
-                    if ( ! hasAllele(gAllele) && gAllele.isCalled() )
-                        throw new IllegalStateException("Allele in genotype " + gAllele + " not in the variant context " + alleles);
-                }
             }
         }
     }

commit 833a0e2cf55df42ce0b6d682137b84b5dc963c97

fix bug in VariantContextBuilder (#1403)
* Fix regression in VariantContextBuilder introduced in #1344 
* Fixes #1404 
* filter validation now fails with IllegalStateException instead of of NPE
* Treat null filter array the same as null filter sets


Co-authored-by: Yossi Farjoun <farjoun@broadinstitute.org>
Co-authored-by: Louis Bergelson <louisb@broadinstitute.org>

src/main/java/htsjdk/variant/variantcontext/VariantContext.java
@@ -393,6 +393,9 @@ public class VariantContext implements Feature, Serializable {
             }
 
             for (String filter : filters) {
+                if ( filter == null) {
+                    throw new IllegalStateException("'null' is not a valid filter string.");
+                }
                 if (!VALID_FILTER.matcher(filter).matches()) {
                     throw new IllegalStateException("Filter '" + filter +
                             "' contains an illegal character. It must conform to the regex ;'" + VALID_FILTER);

src/main/java/htsjdk/variant/variantcontext/VariantContextBuilder.java
@@ -34,7 +34,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.EnumSet;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
@@ -354,7 +353,7 @@ public class VariantContextBuilder {
         if (!filtersCanBeModified) {
             this.filtersCanBeModified = true;
             final Set<String> tempFilters = filters;
-            this.filters = new HashSet<>();
+            this.filters = new LinkedHashSet<>();
             if (tempFilters != null) {
                 this.filters.addAll(tempFilters);
             }
@@ -376,7 +375,7 @@ public class VariantContextBuilder {
             unfiltered();
         } else {
             this.filtersCanBeModified = true;
-            filtersAsIs(new HashSet<>(filters));
+            filtersAsIs(new LinkedHashSet<>(filters));
         }
         return this;
     }
@@ -398,11 +397,16 @@ public class VariantContextBuilder {
     /**
      * {@link #filters}
      *
-     * @param filters  Set of strings to set as the filters for this builder
+     * @param filters  Strings to set as the filters for this builder
      * @return this builder
      */
     public VariantContextBuilder filters(final String ... filters) {
-        filtersAsIs(new LinkedHashSet<>(Arrays.asList(filters)));
+        if(filters == null){
+            this.unfiltered();
+        } else {
+            this.filtersCanBeModified = true;
+            filtersAsIs(new LinkedHashSet<>(Arrays.asList(filters)));
+        }
         return this;
     }
 
@@ -435,6 +439,7 @@ public class VariantContextBuilder {
      */
     public VariantContextBuilder unfiltered() {
         this.filters = null;
+        this.filtersCanBeModified = false;
         return this;
     }
 

src/main/java/htsjdk/variant/vcf/VCFEncoder.java
@@ -44,7 +44,9 @@ public class VCFEncoder {
      * allowing missing fields in the header.
      */
     public VCFEncoder(final VCFHeader header, final boolean allowMissingFieldsInHeader, final boolean outputTrailingFormatFields) {
-        if (header == null) throw new NullPointerException("The VCF header must not be null.");
+        if (header == null) {
+            throw new NullPointerException("The VCF header must not be null.");
+        }
         this.header = header;
         this.allowMissingFieldsInHeader = allowMissingFieldsInHeader;
         this.outputTrailingFormatFields = outputTrailingFormatFields;
@@ -128,8 +130,11 @@ public class VCFEncoder {
         vcfOutput.append(VCFConstants.FIELD_SEPARATOR);
 
         // QUAL
-        if ( ! context.hasLog10PError()) vcfOutput.append(VCFConstants.MISSING_VALUE_v4);
-        else vcfOutput.append(formatQualValue(context.getPhredScaledQual()));
+        if ( !context.hasLog10PError()) {
+            vcfOutput.append(VCFConstants.MISSING_VALUE_v4);
+        } else {
+            vcfOutput.append(formatQualValue(context.getPhredScaledQual()));
+        }
         vcfOutput.append(VCFConstants.FIELD_SEPARATOR)
                 // FILTER
                 .append(getFilterString(context)).append(VCFConstants.FIELD_SEPARATOR);
@@ -137,11 +142,14 @@ public class VCFEncoder {
         // INFO
         final Map<String, String> infoFields = new TreeMap<>();
         for (final Map.Entry<String, Object> field : context.getAttributes().entrySet()) {
-            if (!this.header.hasInfoLine(field.getKey()))
+            if (!this.header.hasInfoLine(field.getKey())) {
                 fieldIsMissingFromHeaderError(context, field.getKey(), "INFO");
+            }
 
             final String outputValue = formatVCFField(field.getValue());
-            if (outputValue != null) infoFields.put(field.getKey(), outputValue);
+            if (outputValue != null) {
+                infoFields.put(field.getKey(), outputValue);
+            }
         }
         writeInfoString(infoFields, vcfOutput);
 
@@ -152,11 +160,12 @@ public class VCFEncoder {
             vcfOutput.append(((LazyGenotypesContext) gc).getUnparsedGenotypeData().toString());
         } else {
             final List<String> genotypeAttributeKeys = context.calcVCFGenotypeKeys(this.header);
-            if ( ! genotypeAttributeKeys.isEmpty()) {
-                for (final String format : genotypeAttributeKeys)
-                    if ( ! this.header.hasFormatLine(format))
+            if ( !genotypeAttributeKeys.isEmpty()) {
+                for (final String format : genotypeAttributeKeys) {
+                    if (!this.header.hasFormatLine(format)) {
                         fieldIsMissingFromHeaderError(context, format, "FORMAT");
-
+                    }
+                }
                 final String genotypeFormatString = ParsingUtils.join(VCFConstants.GENOTYPE_FIELD_SEPARATOR, genotypeAttributeKeys);
 
                 vcfOutput.append(VCFConstants.FIELD_SEPARATOR);
@@ -166,8 +175,6 @@ public class VCFEncoder {
                 appendGenotypeData(context, alleleStrings, genotypeAttributeKeys, vcfOutput);
             }
         }
-
-
     }
 
     VCFHeader getVCFHeader() {
@@ -181,52 +188,60 @@ public class VCFEncoder {
     private String getFilterString(final VariantContext vc) {
         if (vc.isFiltered()) {
             for (final String filter : vc.getFilters()) {
-                if (!this.header.hasFilterLine(filter)) fieldIsMissingFromHeaderError(vc, filter, "FILTER");
+                if (!this.header.hasFilterLine(filter)) {
+                    fieldIsMissingFromHeaderError(vc, filter, "FILTER");
+                }
             }
 
             return ParsingUtils.join(";", ParsingUtils.sortList(vc.getFilters()));
-        } else if (vc.filtersWereApplied()) return VCFConstants.PASSES_FILTERS_v4;
-        else return VCFConstants.UNFILTERED;
+        } else {
+            return vc.filtersWereApplied() ? VCFConstants.PASSES_FILTERS_v4 : VCFConstants.UNFILTERED;
+        }
     }
 
     private static String formatQualValue(final double qual) {
         String s = String.format(QUAL_FORMAT_STRING, qual);
-        if (s.endsWith(QUAL_FORMAT_EXTENSION_TO_TRIM))
+        if (s.endsWith(QUAL_FORMAT_EXTENSION_TO_TRIM)) {
             s = s.substring(0, s.length() - QUAL_FORMAT_EXTENSION_TO_TRIM.length());
+        }
         return s;
     }
 
     private void fieldIsMissingFromHeaderError(final VariantContext vc, final String id, final String field) {
-        if (!allowMissingFieldsInHeader)
+        if (!allowMissingFieldsInHeader) {
             throw new IllegalStateException("Key " + id + " found in VariantContext field " + field
-                                                    + " at " + vc.getContig() + ":" + vc.getStart()
-                                                    + " but this key isn't defined in the VCFHeader.  We require all VCFs to have"
-                                                    + " complete VCF headers by default.");
+                    + " at " + vc.getContig() + ":" + vc.getStart()
+                    + " but this key isn't defined in the VCFHeader.  We require all VCFs to have"
+                    + " complete VCF headers by default.");
+        }
     }
 
     @SuppressWarnings("rawtypes")
     String formatVCFField(final Object val) {
         final String result;
-        if ( val == null )
+        if (val == null) {
             result = VCFConstants.MISSING_VALUE_v4;
-        else if ( val instanceof Double )
+        } else if (val instanceof Double) {
             result = formatVCFDouble((Double) val);
-        else if ( val instanceof Boolean )
-            result = (Boolean)val ? "" : null; // empty string for true, null for false
-        else if ( val instanceof List ) {
-            result = formatVCFField(((List)val).toArray());
-        } else if ( val.getClass().isArray() ) {
+        } else if (val instanceof Boolean) {
+            result = (Boolean) val ? "" : null; // empty string for true, null for false
+        } else if (val instanceof List) {
+            result = formatVCFField(((List) val).toArray());
+        } else if (val.getClass().isArray()) {
             final int length = Array.getLength(val);
-            if ( length == 0 )
+            if (length == 0) {
                 return formatVCFField(null);
-            final StringBuilder sb = new StringBuilder(formatVCFField(Array.get(val, 0)));
-            for ( int i = 1; i < length; i++) {
+            }
+            final StringBuilder sb = new StringBuilder(
+                formatVCFField(Array.get(val, 0)));
+            for (int i = 1; i < length; i++) {
                 sb.append(',');
                 sb.append(formatVCFField(Array.get(val, i)));
             }
             result = sb.toString();
-        } else
+        } else {
             result = val.toString();
+        }
 
         return result;
     }
@@ -245,9 +260,9 @@ public class VCFEncoder {
         final String format;
         if (d < 1) {
             if (d < 0.01) {
-                if (Math.abs(d) >= 1e-20)
+                if (Math.abs(d) >= 1e-20) {
                     format = "%.3e";
-                else {
+                } else {
                     // return a zero format
                     return "0.00";
                 }
@@ -299,7 +314,9 @@ public class VCFEncoder {
             vcfoutput.append(VCFConstants.FIELD_SEPARATOR);
 
             Genotype g = vc.getGenotype(sample);
-            if (g == null) g = GenotypeBuilder.createMissing(sample, ploidy);
+            if (g == null) {
+                g = GenotypeBuilder.createMissing(sample, ploidy);
+            }
 
             final List<String> attrs = new ArrayList<>(genotypeFormatKeys.size());
             for (final String field : genotypeFormatKeys) {
@@ -323,11 +340,11 @@ public class VCFEncoder {
                         final IntGenotypeFieldAccessors.Accessor accessor = GENOTYPE_FIELD_ACCESSORS.getAccessor(field);
                         if (accessor != null) {
                             final int[] intValues = accessor.getValues(g);
-                            if (intValues == null)
+                            if (intValues == null) {
                                 outputValue = VCFConstants.MISSING_VALUE_v4;
-                            else if (intValues.length == 1) // fast path
+                            } else if (intValues.length == 1) { // fast path
                                 outputValue = Integer.toString(intValues[0]);
-                            else {
+                            } else {
                                 final StringBuilder sb = new StringBuilder();
                                 sb.append(intValues[0]);
                                 for (int i = 1; i < intValues.length; i++) {
@@ -342,16 +359,20 @@ public class VCFEncoder {
                         }
                     }
 
-                    if (outputValue != null)
+                    if (outputValue != null) {
                         attrs.add(outputValue);
+                    }
                 }
             }
 
             // strip off trailing missing values
             if (!outputTrailingFormatFields) {
                 for (int i = attrs.size() - 1; i >= 0; i--) {
-                    if (isMissingValue(attrs.get(i))) attrs.remove(i);
-                    else break;
+                    if (isMissingValue(attrs.get(i))) {
+                        attrs.remove(i);
+                    } else {
+                        break;
+                    }
                 }
             }
 
@@ -375,8 +396,11 @@ public class VCFEncoder {
 
         boolean isFirst = true;
         for (final Map.Entry<String, String> entry : infoFields.entrySet()) {
-            if (isFirst) isFirst = false;
-            else vcfoutput.append(VCFConstants.INFO_FIELD_SEPARATOR);
+            if (isFirst) {
+                isFirst = false;
+            } else {
+                vcfoutput.append(VCFConstants.INFO_FIELD_SEPARATOR);
+            }
 
             vcfoutput.append(entry.getKey());
 

src/test/java/htsjdk/variant/variantcontext/VariantContextBuilderTest.java
@@ -314,4 +314,31 @@ public class VariantContextBuilderTest extends VariantBaseTest {
 
         Assert.assertNotEquals(vc2.getGenotypes(), vc1.getGenotypes(), "The two genotype lists should be different. only saw " + vc1.getGenotypes().toString());
     }
+
+    @Test
+    public void testCanResetFilters() {
+        final VariantContextBuilder builder = new VariantContextBuilder("source", "contig", 1, 1, Arrays.asList(Tref, C, G)).filter("TEST");
+        builder.unfiltered();
+        builder.filter("mayIPlease?");
+    }
+
+    @Test(expectedExceptions = IllegalStateException.class)
+    public void testCantCreateNullFilter(){
+        final VariantContextBuilder builder = new VariantContextBuilder("source", "contig", 1, 1, Arrays.asList(Tref, C, G)).filter("TEST");
+        builder.filters((String)null);
+        builder.make();
+    }
+
+    @Test
+    public void testNullFilterArray(){
+        final VariantContextBuilder builder = new VariantContextBuilder("source", "contig", 1, 1, Arrays.asList(Tref, C, G)).filter("TEST");
+        builder.filters((String[])null);
+    }
+
+    @Test
+    public void testNullFilterSet(){
+        final VariantContextBuilder builder = new VariantContextBuilder("source", "contig", 1, 1, Arrays.asList(Tref, C, G)).filter("TEST");
+        builder.filters((Set<String>)null);
+        builder.make();
+    }
 }