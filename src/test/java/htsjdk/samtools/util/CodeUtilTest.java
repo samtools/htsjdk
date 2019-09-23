7e12947a8c35bfc41af791efd5eeb4350672e578
Yossi Farjoun <farjoun@broadinstitute.org>
2019-09-23 09:58:28 -0400

    - added javadoc, license, test

--- src/test/java/htsjdk/samtools/util/CodeUtilTest.java
+++ src/test/java/htsjdk/samtools/util/CodeUtilTest.java
@@ -4,6 +4,8 @@ import htsjdk.HtsjdkTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class CodeUtilTest extends HtsjdkTest {
 
     @Test
@@ -12,5 +14,20 @@ public class CodeUtilTest extends HtsjdkTest {
         Assert.assertEquals(CodeUtil.getOrElse(notNull, null), notNull);
         Assert.assertEquals(CodeUtil.getOrElse(null, notNull), notNull);
         Assert.assertEquals((Object) CodeUtil.getOrElse(null, null), (Object) null);
+    }
+
+    @Test
+    public void applyIfNotNullNegativeTest() {
+        CodeUtil.applyIfNotNull((Object) null, (o) -> Assert.fail("this shouldn't have been called"));
+        CodeUtil.applyIfNotNull((Integer) null, (o) -> Assert.fail("this shouldn't have been called"));
+        CodeUtil.applyIfNotNull((Double) null, (o) -> Assert.fail("this shouldn't have been called"));
+        CodeUtil.applyIfNotNull((String) null, (o) -> Assert.fail("this shouldn't have been called"));
+        CodeUtil.applyIfNotNull((List<Object>) null, (o) -> Assert.fail("this shouldn't have been called"));
+    }
+
+    @Test(expectedExceptions = AssertionError.class)
+    public void applyIfNotNullPositiveTest() {
+        CodeUtil.applyIfNotNull(1, (o) -> Assert.assertNotEquals(o, 1,
+                "Throwing this proves that the method was called with value 1"));
     }
 }
