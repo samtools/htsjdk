eec934838d857245016c6344400cff08e8bd905b
Jim Robinson <jrobinso@broadinstitute.org>
2019-10-11 12:51:19 -0700

    Update comment

--- src/main/java/htsjdk/tribble/util/URLHelperFactory.java
+++ src/main/java/htsjdk/tribble/util/URLHelperFactory.java
@@ -3,15 +3,17 @@ package htsjdk.tribble.util;
 import java.net.URL;
 
 /**
- * A factory for creating {@link  URLHelper} instances.
+ * A factory for creating {@link  URLHelper} instances.  The factory contains a single function
+ * @see #getHelper(URL) which should return a <code>URLHelper</code> instance for the given URL.
  */
 public interface URLHelperFactory {
 
     /**
      * @param url
      * @return a {@link URLHelper} object for the given URL
+     * @throws
      */
-    default URLHelper getHelper(URL url) {
+    default URLHelper getHelper(URL url)   {
         return new RemoteURLHelper(url);
     }
 
