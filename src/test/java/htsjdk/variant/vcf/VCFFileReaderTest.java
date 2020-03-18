fb3035aec1da400e7d6e2fcb74fbef8dd54d816f
Yossi Farjoun <farjoun@broadinstitute.org>
2020-03-18 14:44:54 -0400

    -oops

--- src/test/java/htsjdk/variant/vcf/VCFFileReaderTest.java
+++ src/test/java/htsjdk/variant/vcf/VCFFileReaderTest.java
@@ -44,36 +44,34 @@ public class VCFFileReaderTest extends HtsjdkTest {
         final String TEST_DATA_DIR = "src/test/resources/htsjdk/variant/";
         return new Object[][]{
                 // various ways to refer to a local file
-//                {TEST_DATA_DIR + "VCF4HeaderTest.vcf", null, false, true},
-//
-//                //testing GCS files:
-//
+                {TEST_DATA_DIR + "VCF4HeaderTest.vcf", null, false, true},
+
 //                // this is almost a vcf, but not quite it's missing the #CHROM line and it has no content...
-//                {TEST_DATA_DIR + "Homo_sapiens_assembly38.tile_db_header.vcf", null, false, false},
-//
+                {TEST_DATA_DIR + "Homo_sapiens_assembly38.tile_db_header.vcf", null, false, false},
+
 //                // test that have indexes
-//                {TEST_DATA_DIR + "test.vcf.bgz", TEST_DATA_DIR + "test.vcf.bgz.tbi", true, true},
-//                {TEST_DATA_DIR + "serialization_test.bcf", TEST_DATA_DIR + "serialization_test.bcf.idx", true, true},
-//                {TEST_DATA_DIR + "test1.vcf", TEST_DATA_DIR + "test1.vcf.idx", true, true},
+                {TEST_DATA_DIR + "test.vcf.bgz", TEST_DATA_DIR + "test.vcf.bgz.tbi", true, true},
+                {TEST_DATA_DIR + "serialization_test.bcf", TEST_DATA_DIR + "serialization_test.bcf.idx", true, true},
+                {TEST_DATA_DIR + "test1.vcf", TEST_DATA_DIR + "test1.vcf.idx", true, true},
 //
 //                // test that lack indexes (should succeed)
-//                {TEST_DATA_DIR + "VcfThatLacksAnIndex.vcf.gz", null, false, true},
+                {TEST_DATA_DIR + "VcfThatLacksAnIndex.vcf.gz", null, false, true},
                 {TEST_DATA_DIR + "VcfThatLacksAnIndex.vcf", null, false, true},
-//                {TEST_DATA_DIR + "VcfThatLacksAnIndex but has a space.vcf", null, false, true},
-//                {TEST_DATA_DIR + "VcfThatLacksAnIndex.bcf", null, false, true},
-//                {TEST_DATA_DIR + "VcfThatLacksAnIndex.vcf.bgz", null, false, true},
+                {TEST_DATA_DIR + "VcfThatLacksAnIndex but has a space.vcf", null, false, true},
+                {TEST_DATA_DIR + "VcfThatLacksAnIndex.bcf", null, false, true},
+                {TEST_DATA_DIR + "VcfThatLacksAnIndex.vcf.bgz", null, false, true},
 //
 //                // test that lack indexes should fail)
-//                {TEST_DATA_DIR + "VcfThatLacksAnIndex.vcf.gz", null, true, false},
-//                {TEST_DATA_DIR + "VcfThatLacksAnIndex.vcf", null, true, false},
-//                {TEST_DATA_DIR + "VcfThatLacksAnIndex.bcf", null, true, false},
-//                {TEST_DATA_DIR + "VcfThatLacksAnIndex.vcf.bgz", null, true, false},
+                {TEST_DATA_DIR + "VcfThatLacksAnIndex.vcf.gz", null, true, false},
+                {TEST_DATA_DIR + "VcfThatLacksAnIndex.vcf", null, true, false},
+                {TEST_DATA_DIR + "VcfThatLacksAnIndex.bcf", null, true, false},
+                {TEST_DATA_DIR + "VcfThatLacksAnIndex.vcf.bgz", null, true, false},
 //
 //                // testing that v4.2 parses Source/Version fields, see issue #517
-//                {TEST_DATA_DIR + "Vcf4.2WithSourceVersionInfoFields.vcf", null, false, true},
+                {TEST_DATA_DIR + "Vcf4.2WithSourceVersionInfoFields.vcf", null, false, true},
 //
 //                // should reject bcf v2.2 on read, see issue https://github.com/samtools/htsjdk/issues/1323
-//                {TEST_DATA_DIR + "BCFVersion22Uncompressed.bcf", null, false, false}
+                {TEST_DATA_DIR + "BCFVersion22Uncompressed.bcf", null, false, false}
         };
     }
 
