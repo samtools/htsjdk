75716e08896e4ed0b8da319d31479b3cd404fce6
Yossi Farjoun <farjoun@broadinstitute.org>
2020-03-24 15:40:36 -0400

    fix a test in  VCFFileReaderTest.java  (#1468)
    * fix a vcfFileReaderTest by making it iterate through entire file instead of just checking the header

--- src/test/java/htsjdk/variant/vcf/VCFFileReaderTest.java
+++ src/test/java/htsjdk/variant/vcf/VCFFileReaderTest.java
@@ -46,33 +46,31 @@ public class VCFFileReaderTest extends HtsjdkTest {
                 // various ways to refer to a local file
                 {TEST_DATA_DIR + "VCF4HeaderTest.vcf", null, false, true},
 
-                //testing GCS files:
-
-                // this is almost a vcf, but not quite it's missing the #CHROM line and it has no content...
+//                // this is almost a vcf, but not quite it's missing the #CHROM line and it has no content...
                 {TEST_DATA_DIR + "Homo_sapiens_assembly38.tile_db_header.vcf", null, false, false},
 
-                // test that have indexes
+//                // test that have indexes
                 {TEST_DATA_DIR + "test.vcf.bgz", TEST_DATA_DIR + "test.vcf.bgz.tbi", true, true},
                 {TEST_DATA_DIR + "serialization_test.bcf", TEST_DATA_DIR + "serialization_test.bcf.idx", true, true},
                 {TEST_DATA_DIR + "test1.vcf", TEST_DATA_DIR + "test1.vcf.idx", true, true},
-
-                // test that lack indexes (should succeed)
+//
+//                // test that lack indexes (should succeed)
                 {TEST_DATA_DIR + "VcfThatLacksAnIndex.vcf.gz", null, false, true},
                 {TEST_DATA_DIR + "VcfThatLacksAnIndex.vcf", null, false, true},
                 {TEST_DATA_DIR + "VcfThatLacksAnIndex but has a space.vcf", null, false, true},
                 {TEST_DATA_DIR + "VcfThatLacksAnIndex.bcf", null, false, true},
                 {TEST_DATA_DIR + "VcfThatLacksAnIndex.vcf.bgz", null, false, true},
-
-                // test that lack indexes should fail)
+//
+//                // test that lack indexes should fail)
                 {TEST_DATA_DIR + "VcfThatLacksAnIndex.vcf.gz", null, true, false},
                 {TEST_DATA_DIR + "VcfThatLacksAnIndex.vcf", null, true, false},
                 {TEST_DATA_DIR + "VcfThatLacksAnIndex.bcf", null, true, false},
                 {TEST_DATA_DIR + "VcfThatLacksAnIndex.vcf.bgz", null, true, false},
-
-                // testing that v4.2 parses Source/Version fields, see issue #517
+//
+//                // testing that v4.2 parses Source/Version fields, see issue #517
                 {TEST_DATA_DIR + "Vcf4.2WithSourceVersionInfoFields.vcf", null, false, true},
-
-                // should reject bcf v2.2 on read, see issue https://github.com/samtools/htsjdk/issues/1323
+//
+//                // should reject bcf v2.2 on read, see issue https://github.com/samtools/htsjdk/issues/1323
                 {TEST_DATA_DIR + "BCFVersion22Uncompressed.bcf", null, false, false}
         };
     }
@@ -82,7 +80,12 @@ public class VCFFileReaderTest extends HtsjdkTest {
         try (FileSystem fs = Jimfs.newFileSystem("test", Configuration.unix())) {
             final Path tribbleFileInJimfs = TestUtils.getTribbleFileInJimfs(file, index, fs);
             try (final VCFFileReader reader = new VCFFileReader(tribbleFileInJimfs, requiresIndex)) {
+
                 final VCFHeader header = reader.getFileHeader();
+                reader.iterator().stream().forEach(
+                        v->v.getGenotypes()
+                                .stream()
+                                .count());
             } catch (Exception e) {
                 if (shouldSucceed) {
                     throw e;
@@ -91,7 +94,7 @@ public class VCFFileReaderTest extends HtsjdkTest {
                 }
             }
         }
-        // fail if a test that should have throw didn't
+        // fail if a test that should have thrown didn't
         Assert.assertTrue(shouldSucceed, "Test should have failed but succeeded");
     }
 }
