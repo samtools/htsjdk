6df0b4161d5cee7c0e7241ce1768aa2874297b2c
Louis Bergelson <louisb@broadinstitute.org>
2020-06-01 11:17:38 -0400

    Improve error message in AbstractRecordAndOffset
    * Include the value of the offset and length in an error message so you know what the out of bounds value was.

--- src/main/java/htsjdk/samtools/util/AbstractRecordAndOffset.java
+++ src/main/java/htsjdk/samtools/util/AbstractRecordAndOffset.java
@@ -144,7 +144,7 @@ public class AbstractRecordAndOffset {
     protected void validateOffset(int offset, final byte[] array) {
         if (offset < 0 || offset >= array.length) {
             throw new IllegalArgumentException("The requested position is not covered by this " + this.getClass().getSimpleName() +
-                    " object.");
+                    " object. \n Offset = " + offset + " Array length = " + array.length + "");
         }
     }
 }
