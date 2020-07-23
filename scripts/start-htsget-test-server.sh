6f83e2d71148a3f9701a5afbde8159c74c49f146
Anders Leung <37423859+andersleung@users.noreply.github.com>
2020-07-23 13:37:59 -0400

    Make start script executable

--- scripts/start-htsget-test-server.sh
+++ scripts/start-htsget-test-server.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 WORKING_DIR=/home/travis/build/samtools/htsjdk
-sudo docker pull ga4gh/htsget-ref:1.1.0
-sudo docker container run -d --name htsget-server -p 3000:3000 --env HTSGET_PORT=3000 --env HTSGET_HOST=http://127.0.0.1:3000 \
+docker pull ga4gh/htsget-ref:1.1.0
+docker container run -d --name htsget-server -p 3000:3000 --env HTSGET_PORT=3000 --env HTSGET_HOST=http://127.0.0.1:3000 \
       -v $WORKING_DIR/src/test/resources/htsjdk/samtools/BAMFileIndexTest/:/data \
       ga4gh/htsget-ref:1.1.0 \
       ./htsref -config /data/htsget_config.json
-sudo docker container ls -a
+docker container ls -a
 
 curl http://localhost:3000
