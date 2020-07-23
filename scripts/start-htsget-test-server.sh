90de6bedfa3e6002d9b4f095a61021d0b94b71ff
Anders Leung <37423859+andersleung@users.noreply.github.com>
2020-07-23 13:17:00 -0400

    sudo docker commands when starting htsget test server

--- scripts/start-htsget-test-server.sh
+++ scripts/start-htsget-test-server.sh
@@ -1,10 +1,10 @@
 WORKING_DIR=/home/travis/build/samtools/htsjdk
 
-docker pull ga4gh/htsget-ref:1.1.0
-docker container run -d --name htsget-server -p 3000:3000 --env HTSGET_PORT=3000 --env HTSGET_HOST=http://127.0.0.1:3000 \
+sudo docker pull ga4gh/htsget-ref:1.1.0
+sudo docker container run -d --name htsget-server -p 3000:3000 --env HTSGET_PORT=3000 --env HTSGET_HOST=http://127.0.0.1:3000 \
       -v $WORKING_DIR/src/test/resources/htsjdk/samtools/BAMFileIndexTest/:/data \
       ga4gh/htsget-ref:1.1.0 \
       ./htsref -config /data/htsget_config.json
-docker container ls -a
+sudo docker container ls -a
 
 curl http://localhost:3000
