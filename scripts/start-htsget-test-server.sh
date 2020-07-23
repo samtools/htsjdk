08fcefaa8eb3058ff4df07f0de0ea1012debb5a2
Anders Leung <37423859+andersleung@users.noreply.github.com>
2020-07-23 13:31:21 -0400

    sudo execute script

--- scripts/start-htsget-test-server.sh
+++ scripts/start-htsget-test-server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-WORKING_DIR=/home/travis/build/samtools/htsjdk
 
+WORKING_DIR=/home/travis/build/samtools/htsjdk
 sudo docker pull ga4gh/htsget-ref:1.1.0
 sudo docker container run -d --name htsget-server -p 3000:3000 --env HTSGET_PORT=3000 --env HTSGET_HOST=http://127.0.0.1:3000 \
       -v $WORKING_DIR/src/test/resources/htsjdk/samtools/BAMFileIndexTest/:/data \
