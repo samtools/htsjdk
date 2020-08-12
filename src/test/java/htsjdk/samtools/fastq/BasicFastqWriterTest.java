c2fca57ea7b5a56d7f5b3de2d7cf5335c2f85278
Daniel Cameron <d-cameron@github.com>
2020-08-12 14:26:41 +1000

    Added BasicFastqWriter test case testing flush() call count

---
+++ src/test/java/htsjdk/samtools/fastq/BasicFastqWriterTest.java
@@ -0,0 +1,42 @@
+package htsjdk.samtools.fastq;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+public class BasicFastqWriterTest {
+    public class OutputStreamWrapper extends ByteArrayOutputStream {
+        public int flushCalled = 0;
+        public int writeCalled = 0;
+        @Override
+        public void flush() throws IOException {
+            flushCalled++;
+            super.flush();
+        }
+
+        @Override
+        public void write(byte[] b) throws IOException {
+            writeCalled++;
+            super.write(b);
+        }
+    }
+
+    /**
+     * #1497
+     */
+    @Test
+    public void testFlushNotSpammed() {
+        OutputStreamWrapper loggedStream = new OutputStreamWrapper();
+        PrintStream ps = new PrintStream(loggedStream);
+        try (BasicFastqWriter fqw = new BasicFastqWriter(ps)) {
+            for (int i = 0; i < 10000; i++) {
+                fqw.write(new FastqRecord("name", "NNNN", null, "...."));
+            }
+        }
+        // flush()/close() results in two flushes
+        Assert.assertTrue(loggedStream.flushCalled <= 2);
+    }
+}
