0aff7ea1ed7143967afd99f2ca32f171d40ce301
Louis Bergelson <louisb@broadinstitute.org>
2020-10-02 14:41:10 -0400

    make script runnable locally and make it repeatable

--- scripts/htsget-scripts/start-htsget-test-server.sh
+++ scripts/htsget-scripts/start-htsget-test-server.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
 set -e
+set -v
 
 DOCKER_NAME='ga4gh/htsget-refserver'
 SERVER_VERSION='1.1.0'
 DOCKER_COORDINATE=${DOCKER_NAME}:${SERVER_VERSION}
 
-WORKING_DIR=/home/travis/build/samtools/htsjdk
 docker pull ${DOCKER_COORDINATE}
+docker rm htsget-server
 docker container run -d --name htsget-server -p 3000:3000 --env HTSGET_PORT=3000 --env HTSGET_HOST=http://127.0.0.1:3000 \
-      -v $WORKING_DIR/src/test/resources/htsjdk/samtools/BAMFileIndexTest/:/data \
-      -v $WORKING_DIR/scripts/htsget-scripts:/data/scripts \
+      -v ${PWD}/src/test/resources/htsjdk/samtools/BAMFileIndexTest/:/data \
+      -v ${PWD}/scripts/htsget-scripts:/data/scripts \
       ${DOCKER_COORDINATE} \
       ./htsref -config /data/scripts/htsget_config.json
+sleep 2
 docker container ls -a
+
 
 curl http://localhost:3000
