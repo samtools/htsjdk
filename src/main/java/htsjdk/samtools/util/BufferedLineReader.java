36e2dc79c03cdb8c6a6db02127ddbd18888c0299
Nils Homer <nh13@users.noreply.github.com>
2021-01-20 12:42:56 -0800

    Update src/main/java/htsjdk/samtools/util/BufferedLineReader.java

--- src/main/java/htsjdk/samtools/util/BufferedLineReader.java
+++ src/main/java/htsjdk/samtools/util/BufferedLineReader.java
@@ -31,7 +31,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.LineNumberReader;
 import java.nio.charset.Charset;
-import java.nio.charset.CharsetEncoder; 
+import java.nio.charset. StandardCharsets; 
 
 /**
  * Implementation of LineReader that is a thin wrapper around BufferedReader.  On Linux, this is faster
