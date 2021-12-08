c0b61a27010aa565be14cb3769ef78f21f857678
Chris Norman <cnorman@broadinstitute.org>
2021-12-08 13:18:04 -0500

    Upgrade to samtools 1.14. (#1583)
    * Upgrade to samtools 1.14.
    * Update old CRAM files that samtools no longer accepts.

--- scripts/install-samtools.sh
+++ scripts/install-samtools.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
-wget https://github.com/samtools/samtools/releases/download/1.10/samtools-1.10.tar.bz2
-tar -xjvf samtools-1.10.tar.bz2
-cd samtools-1.10 && ./configure --prefix=/usr && make && sudo make install
+wget https://github.com/samtools/samtools/releases/download/1.14/samtools-1.14.tar.bz2
+tar -xjvf samtools-1.14.tar.bz2
+cd samtools-1.14 && ./configure --prefix=/usr && make && sudo make install
