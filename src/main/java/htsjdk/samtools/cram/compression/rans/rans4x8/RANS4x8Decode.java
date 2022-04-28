58c14cd99c29b4a7bd734b389f8c3faea547694a
yash-puligundla <yasasvini.puligundla@ga4gh.org>
2022-04-28 11:33:12 -0400

    Move/inline RANS 4x8 D04 uncompress method into RANS4x8Decode.

--- src/main/java/htsjdk/samtools/cram/compression/rans/rans4x8/RANS4x8Decode.java
+++ src/main/java/htsjdk/samtools/cram/compression/rans/rans4x8/RANS4x8Decode.java
@@ -5,6 +5,7 @@ import htsjdk.samtools.cram.compression.rans.Constants;
 import htsjdk.samtools.cram.compression.rans.RANSDecode;
 import htsjdk.samtools.cram.compression.rans.RANSDecodingSymbol;
 import htsjdk.samtools.cram.compression.rans.RANSParams;
+import htsjdk.samtools.cram.compression.rans.Utils;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
@@ -51,7 +52,78 @@ public class RANS4x8Decode extends RANSDecode {
     private ByteBuffer uncompressOrder0Way4(final ByteBuffer inBuffer, final ByteBuffer outBuffer) {
         // read the frequency table. using the frequency table, set the values of RANSDecodingSymbols
         readStatsOrder0(inBuffer);
-        D04.uncompress(inBuffer, getD()[0], getDecodingSymbols()[0], outBuffer);
+//        D04.uncompress(inBuffer, getD()[0], getDecodingSymbols()[0], outBuffer);
+        final ArithmeticDecoder D = getD()[0];
+        final RANSDecodingSymbol[] syms = getDecodingSymbols()[0];
+
+        int rans0, rans1, rans2, rans3;
+        rans0 = inBuffer.getInt();
+        rans1 = inBuffer.getInt();
+        rans2 = inBuffer.getInt();
+        rans3 = inBuffer.getInt();
+
+        final int out_sz = outBuffer.remaining();
+        final int out_end = (out_sz & ~3);
+        for (int i = 0; i < out_end; i += 4) {
+            final byte c0 = D.reverseLookup[Utils.RANSGetCumulativeFrequency(rans0, Constants.TOTAL_FREQ_SHIFT)];
+            final byte c1 = D.reverseLookup[Utils.RANSGetCumulativeFrequency(rans1, Constants.TOTAL_FREQ_SHIFT)];
+            final byte c2 = D.reverseLookup[Utils.RANSGetCumulativeFrequency(rans2, Constants.TOTAL_FREQ_SHIFT)];
+            final byte c3 = D.reverseLookup[Utils.RANSGetCumulativeFrequency(rans3, Constants.TOTAL_FREQ_SHIFT)];
+
+            outBuffer.put(i, c0);
+            outBuffer.put(i + 1, c1);
+            outBuffer.put(i + 2, c2);
+            outBuffer.put(i + 3, c3);
+
+            rans0 = syms[0xFF & c0].advanceSymbolStep(rans0, Constants.TOTAL_FREQ_SHIFT);
+            rans1 = syms[0xFF & c1].advanceSymbolStep(rans1, Constants.TOTAL_FREQ_SHIFT);
+            rans2 = syms[0xFF & c2].advanceSymbolStep(rans2, Constants.TOTAL_FREQ_SHIFT);
+            rans3 = syms[0xFF & c3].advanceSymbolStep(rans3,  Constants.TOTAL_FREQ_SHIFT);
+
+            rans0 = Utils.RANSDecodeRenormalize4x8(rans0, inBuffer);
+            rans1 = Utils.RANSDecodeRenormalize4x8(rans1, inBuffer);
+            rans2 = Utils.RANSDecodeRenormalize4x8(rans2, inBuffer);
+            rans3 = Utils.RANSDecodeRenormalize4x8(rans3, inBuffer);
+        }
+
+        outBuffer.position(out_end);
+        byte c;
+        switch (out_sz & 3) {
+            case 0:
+                break;
+
+            case 1:
+                c = D.reverseLookup[Utils.RANSGetCumulativeFrequency(rans0, Constants.TOTAL_FREQ_SHIFT)];
+                syms[0xFF & c].advanceSymbol4x8(rans0, inBuffer, Constants.TOTAL_FREQ_SHIFT);
+                outBuffer.put(c);
+                break;
+
+            case 2:
+                c = D.reverseLookup[Utils.RANSGetCumulativeFrequency(rans0, Constants.TOTAL_FREQ_SHIFT)];
+                syms[0xFF & c].advanceSymbol4x8(rans0, inBuffer, Constants.TOTAL_FREQ_SHIFT);
+                outBuffer.put(c);
+
+                c = D.reverseLookup[Utils.RANSGetCumulativeFrequency(rans1, Constants.TOTAL_FREQ_SHIFT)];
+                syms[0xFF & c].advanceSymbol4x8(rans1, inBuffer, Constants.TOTAL_FREQ_SHIFT);
+                outBuffer.put(c);
+                break;
+
+            case 3:
+                c = D.reverseLookup[Utils.RANSGetCumulativeFrequency(rans0, Constants.TOTAL_FREQ_SHIFT)];
+                syms[0xFF & c].advanceSymbol4x8(rans0, inBuffer,  Constants.TOTAL_FREQ_SHIFT);
+                outBuffer.put(c);
+
+                c = D.reverseLookup[Utils.RANSGetCumulativeFrequency(rans1, Constants.TOTAL_FREQ_SHIFT)];
+                syms[0xFF & c].advanceSymbol4x8(rans1, inBuffer, Constants.TOTAL_FREQ_SHIFT);
+                outBuffer.put(c);
+
+                c = D.reverseLookup[Utils.RANSGetCumulativeFrequency(rans2, Constants.TOTAL_FREQ_SHIFT)];
+                syms[0xFF & c].advanceSymbol4x8(rans2, inBuffer, Constants.TOTAL_FREQ_SHIFT);
+                outBuffer.put(c);
+                break;
+        }
+
+        outBuffer.position(0);
         return outBuffer;
     }
 
