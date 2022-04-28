58f76a8af671b14193f8b858c052bd36d8b3a1f2
yash-puligundla <yasasvini.puligundla@ga4gh.org>
2022-04-28 11:22:55 -0400

    Move/inline RANS 4x8 E14 compress method into RANS4x8Encode.

--- src/main/java/htsjdk/samtools/cram/compression/rans/rans4x8/RANS4x8Encode.java
+++ src/main/java/htsjdk/samtools/cram/compression/rans/rans4x8/RANS4x8Encode.java
@@ -127,7 +127,84 @@ public class RANS4x8Encode extends RANSEncode<RANS4x8Params> {
         final int frequencyTableSize = writeFrequenciesOrder1(cp, F);
 
         inBuffer.rewind();
-        final int compressedBlobSize = E14.compress(inBuffer, getEncodingSymbols(), cp);
+
+        final RANSEncodingSymbol[][] syms = getEncodingSymbols();
+        final int in_size = inBuffer.remaining();
+        final int compressedBlobSize;
+        int rans0, rans1, rans2, rans3;
+        rans0 = Constants.RANS_4x8_LOWER_BOUND;
+        rans1 = Constants.RANS_4x8_LOWER_BOUND;
+        rans2 = Constants.RANS_4x8_LOWER_BOUND;
+        rans3 = Constants.RANS_4x8_LOWER_BOUND;
+
+        /*
+         * Slicing is needed for buffer reversing later.
+         */
+        final ByteBuffer ptr = cp.slice();
+
+        final int isz4 = in_size >> 2;
+        int i0 = isz4 - 2;
+        int i1 = 2 * isz4 - 2;
+        int i2 = 3 * isz4 - 2;
+        int i3 = 4 * isz4 - 2;
+
+        int l0 = 0;
+        if (i0 + 1 >= 0) {
+            l0 = 0xFF & inBuffer.get(i0 + 1);
+        }
+        int l1 = 0;
+        if (i1 + 1 >= 0) {
+            l1 = 0xFF & inBuffer.get(i1 + 1);
+        }
+        int l2 = 0;
+        if (i2 + 1 >= 0) {
+            l2 = 0xFF & inBuffer.get(i2 + 1);
+        }
+        int l3;
+
+        // Deal with the remainder
+        l3 = 0xFF & inBuffer.get(in_size - 1);
+        for (i3 = in_size - 2; i3 > 4 * isz4 - 2 && i3 >= 0; i3--) {
+            final int c3 = 0xFF & inBuffer.get(i3);
+            rans3 = syms[c3][l3].putSymbol4x8(rans3, ptr);
+            l3 = c3;
+        }
+
+        for (; i0 >= 0; i0--, i1--, i2--, i3--) {
+            final int c0 = 0xFF & inBuffer.get(i0);
+            final int c1 = 0xFF & inBuffer.get(i1);
+            final int c2 = 0xFF & inBuffer.get(i2);
+            final int c3 = 0xFF & inBuffer.get(i3);
+
+            rans3 = syms[c3][l3].putSymbol4x8(rans3, ptr);
+            rans2 = syms[c2][l2].putSymbol4x8(rans2, ptr);
+            rans1 = syms[c1][l1].putSymbol4x8(rans1, ptr);
+            rans0 = syms[c0][l0].putSymbol4x8(rans0, ptr);
+
+            l0 = c0;
+            l1 = c1;
+            l2 = c2;
+            l3 = c3;
+        }
+
+        rans3 = syms[0][l3].putSymbol4x8(rans3, ptr);
+        rans2 = syms[0][l2].putSymbol4x8(rans2, ptr);
+        rans1 = syms[0][l1].putSymbol4x8(rans1, ptr);
+        rans0 = syms[0][l0].putSymbol4x8(rans0, ptr);
+
+        ptr.order(ByteOrder.BIG_ENDIAN);
+        ptr.putInt(rans3);
+        ptr.putInt(rans2);
+        ptr.putInt(rans1);
+        ptr.putInt(rans0);
+        ptr.flip();
+        compressedBlobSize = ptr.limit();
+        Utils.reverse(ptr);
+        /*
+         * Depletion of the in buffer cannot be confirmed because of the get(int
+         * position) method use during encoding, hence enforcing:
+         */
+        inBuffer.position(inBuffer.limit());
 
         // write the prefix at the beginning of the output buffer
         writeCompressionPrefix(RANSParams.ORDER.ONE, outBuffer, inSize, frequencyTableSize, compressedBlobSize);
