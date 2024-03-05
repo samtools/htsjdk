222d10b4e7be763453e2a0a1886cbec99e15e886
Chris Norman <cnorman@broadinstitute.org>
2024-03-04 19:05:20 -0500

    Upgrade to samtools 1.19.1 for interop testing. (#1706)
    * Upgrade to samtools 1.19.1 for interop testing.

--- scripts/install-samtools.sh
+++ scripts/install-samtools.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
-wget https://github.com/samtools/samtools/releases/download/1.14/samtools-1.14.tar.bz2
-tar -xjvf samtools-1.14.tar.bz2
-cd samtools-1.14 && ./configure --prefix=/usr && make && sudo make install
+wget https://github.com/samtools/samtools/releases/download/1.19.1/samtools-1.19.1.tar.bz2
+tar -xjvf samtools-1.19.1.tar.bz2
+cd samtools-1.19.1 && ./configure --prefix=/usr && make && sudo make install
