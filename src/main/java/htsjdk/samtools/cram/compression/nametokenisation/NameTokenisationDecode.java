7e57f10fbadbc4bd78e92bf174bef5e68e9a25bc
Chris Norman <cnorman@broadinstitute.org>
2024-11-13 16:38:41 -0500

    Name tokenization codec fixes.

--- src/main/java/htsjdk/samtools/cram/compression/nametokenisation/NameTokenisationDecode.java
+++ src/main/java/htsjdk/samtools/cram/compression/nametokenisation/NameTokenisationDecode.java
@@ -7,22 +7,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringJoiner;
 
-import static htsjdk.samtools.cram.compression.nametokenisation.TokenStreams.TOKEN_TYPE;
-import static htsjdk.samtools.cram.compression.nametokenisation.TokenStreams.TOKEN_STRING;
-import static htsjdk.samtools.cram.compression.nametokenisation.TokenStreams.TOKEN_CHAR;
-import static htsjdk.samtools.cram.compression.nametokenisation.TokenStreams.TOKEN_DIGITS0;
-import static htsjdk.samtools.cram.compression.nametokenisation.TokenStreams.TOKEN_DZLEN;
-import static htsjdk.samtools.cram.compression.nametokenisation.TokenStreams.TOKEN_DUP;
-import static htsjdk.samtools.cram.compression.nametokenisation.TokenStreams.TOKEN_DIGITS;
-import static htsjdk.samtools.cram.compression.nametokenisation.TokenStreams.TOKEN_DELTA;
-import static htsjdk.samtools.cram.compression.nametokenisation.TokenStreams.TOKEN_DELTA0;
-import static htsjdk.samtools.cram.compression.nametokenisation.TokenStreams.TOKEN_MATCH;
-import static htsjdk.samtools.cram.compression.nametokenisation.TokenStreams.TOKEN_END;
 
 public class NameTokenisationDecode {
 
     public static String uncompress(final ByteBuffer inBuffer) {
-        return uncompress(inBuffer, "\n");
+        //TODO: make this stop sentinel into a shared static constant
+        // Actually, this doesn't need to be exposed as an arg on this, so move it into the uncompress method
+        return uncompress(inBuffer, "\0");
     }
 
     public static String uncompress(
@@ -32,16 +23,16 @@ public class NameTokenisationDecode {
         final int uncompressedLength =  inBuffer.getInt() & 0xFFFFFFFF; //unused variable. Following the spec
         final int numNames =  inBuffer.getInt() & 0xFFFFFFFF;
         final int useArith = inBuffer.get() & 0xFF;
-        TokenStreams tokenStreams = new TokenStreams(inBuffer, useArith, numNames);
-        List<List<String>> tokensList = new ArrayList<>(numNames);
+        final TokenStreams tokenStreams = new TokenStreams(inBuffer, useArith, numNames);
+        final List<List<String>> tokensList = new ArrayList<>(numNames);
         for(int i = 0; i < numNames; i++) {
             tokensList.add(new ArrayList<>());
         }
-        StringJoiner decodedNamesJoiner = new StringJoiner(separator);
+        final StringJoiner decodedNamesJoiner = new StringJoiner(separator);
         for (int i = 0; i < numNames; i++) {
             decodedNamesJoiner.add(decodeSingleName(tokenStreams, tokensList, i));
         }
-        String uncompressedNames = decodedNamesJoiner.toString();
+        final String uncompressedNames = decodedNamesJoiner.toString();
         if (uncompressedLength == uncompressedNames.length() + separator.length()){
             return uncompressedNames + separator;
         }
@@ -55,11 +46,11 @@ public class NameTokenisationDecode {
 
         // The information about whether a name is a duplicate or not
         // is obtained from the list of tokens at tokenStreams[0,0]
-        byte nameType = tokenStreams.getTokenStreamByteBuffer(0,TOKEN_TYPE).get();
+        final byte nameType = tokenStreams.getTokenStreamByteBuffer(0,TokenStreams.TOKEN_TYPE).get();
         final ByteBuffer distBuffer = tokenStreams.getTokenStreamByteBuffer(0,nameType).order(ByteOrder.LITTLE_ENDIAN);
         final int dist = distBuffer.getInt() & 0xFFFFFFFF;
         final int prevNameIndex = currentNameIndex - dist;
-        if (nameType == TOKEN_DUP){
+        if (nameType == TokenStreams.TOKEN_DUP){
             tokensList.add(currentNameIndex, tokensList.get(prevNameIndex));
             return String.join("", tokensList.get(currentNameIndex));
         }
@@ -67,33 +58,33 @@ public class NameTokenisationDecode {
         byte type;
         StringBuilder decodedNameBuilder = new StringBuilder();
         do {
-            type = tokenStreams.getTokenStreamByteBuffer(tokenPosition, TOKEN_TYPE).get();
+            type = tokenStreams.getTokenStreamByteBuffer(tokenPosition, TokenStreams.TOKEN_TYPE).get();
             String currentToken = "";
             switch(type){
-                case TOKEN_CHAR:
-                    final char currentTokenChar = (char) tokenStreams.getTokenStreamByteBuffer(tokenPosition, TOKEN_CHAR).get();
+                case TokenStreams.TOKEN_CHAR:
+                    final char currentTokenChar = (char) tokenStreams.getTokenStreamByteBuffer(tokenPosition, TokenStreams.TOKEN_CHAR).get();
                     currentToken = String.valueOf(currentTokenChar);
                     break;
-                case TOKEN_STRING:
-                    currentToken = readString(tokenStreams.getTokenStreamByteBuffer(tokenPosition, TOKEN_STRING));
+                case TokenStreams.TOKEN_STRING:
+                    currentToken = readString(tokenStreams.getTokenStreamByteBuffer(tokenPosition, TokenStreams.TOKEN_STRING));
                     break;
-                case TOKEN_DIGITS:
-                    currentToken = getDigitsToken(tokenStreams, tokenPosition, TOKEN_DIGITS);
+                case TokenStreams.TOKEN_DIGITS:
+                    currentToken = getDigitsToken(tokenStreams, tokenPosition, TokenStreams.TOKEN_DIGITS);
                     break;
-                case TOKEN_DIGITS0:
-                    final String digits0Token = getDigitsToken(tokenStreams, tokenPosition, TOKEN_DIGITS0);
-                    final int lenDigits0Token = tokenStreams.getTokenStreamByteBuffer(tokenPosition, TOKEN_DZLEN).get() & 0xFF;
+                case TokenStreams.TOKEN_DIGITS0:
+                    final String digits0Token = getDigitsToken(tokenStreams, tokenPosition, TokenStreams.TOKEN_DIGITS0);
+                    final int lenDigits0Token = tokenStreams.getTokenStreamByteBuffer(tokenPosition, TokenStreams.TOKEN_DZLEN).get() & 0xFF;
                     currentToken = leftPadNumber(digits0Token, lenDigits0Token);
                     break;
-                case TOKEN_DELTA:
-                    currentToken = getDeltaToken(tokenStreams, tokenPosition, tokensList, prevNameIndex, TOKEN_DELTA);
+                case TokenStreams.TOKEN_DELTA:
+                    currentToken = getDeltaToken(tokenStreams, tokenPosition, tokensList, prevNameIndex, TokenStreams.TOKEN_DELTA);
                     break;
-                case TOKEN_DELTA0:
-                    final String delta0Token = getDeltaToken(tokenStreams, tokenPosition, tokensList, prevNameIndex, TOKEN_DELTA0);
+                case TokenStreams.TOKEN_DELTA0:
+                    final String delta0Token = getDeltaToken(tokenStreams, tokenPosition, tokensList, prevNameIndex, TokenStreams.TOKEN_DELTA0);
                     final int lenDelta0Token = tokensList.get(prevNameIndex).get(tokenPosition-1).length();
                     currentToken = leftPadNumber(delta0Token, lenDelta0Token);
                     break;
-                case TOKEN_MATCH:
+                case TokenStreams.TOKEN_MATCH:
                     currentToken = tokensList.get(prevNameIndex).get(tokenPosition-1);
                     break;
                 default:
@@ -102,9 +93,9 @@ public class NameTokenisationDecode {
             tokensList.get(currentNameIndex).add(tokenPosition-1,currentToken);
             decodedNameBuilder.append(currentToken);
             tokenPosition++;
-        } while (type!= TOKEN_END);
+        } while (type!= TokenStreams.TOKEN_END);
         return decodedNameBuilder.toString();
-        }
+    }
 
     private static String getDeltaToken(
             final TokenStreams tokenStreams,
@@ -112,7 +103,7 @@ public class NameTokenisationDecode {
             final List<List<String>> tokensList,
             final int prevNameIndex,
             final byte tokenType) {
-        if (!(tokenType == TOKEN_DELTA || tokenType == TOKEN_DELTA0)){
+        if (!(tokenType == TokenStreams.TOKEN_DELTA || tokenType == TokenStreams.TOKEN_DELTA0)){
             throw new CRAMException(String.format("Invalid tokenType : %s. " +
                     "tokenType must be either TOKEN_DELTA or TOKEN_DELTA0", tokenType));
         }
@@ -120,7 +111,7 @@ public class NameTokenisationDecode {
         try {
             prevToken = Integer.parseInt(tokensList.get(prevNameIndex).get(tokenPosition -1));
         } catch (final NumberFormatException e) {
-            final String exceptionMessageSubstring = (tokenType == TOKEN_DELTA) ? "DIGITS or DELTA" : "DIGITS0 or DELTA0";
+            final String exceptionMessageSubstring = (tokenType == TokenStreams.TOKEN_DELTA) ? "DIGITS or DELTA" : "DIGITS0 or DELTA0";
             throw new CRAMException(String.format("The token in the prior name must be of type %s",
                     exceptionMessageSubstring), e);
         }
@@ -132,7 +123,7 @@ public class NameTokenisationDecode {
             final TokenStreams tokenStreams,
             final int tokenPosition,
             final byte tokenType ) {
-        if (!(tokenType == TOKEN_DIGITS || tokenType == TOKEN_DIGITS0)){
+        if (!(tokenType == TokenStreams.TOKEN_DIGITS || tokenType == TokenStreams.TOKEN_DIGITS0)){
             throw new CRAMException(String.format("Invalid tokenType : %s. " +
                     "tokenType must be either TOKEN_DIGITS or TOKEN_DIGITS0", tokenType));
         }
--- src/test/java/htsjdk/samtools/cram/NameTokenizationInteropTest.java
+++ src/test/java/htsjdk/samtools/cram/NameTokenizationInteropTest.java
@@ -22,24 +22,23 @@ import java.util.List;
 public class NameTokenizationInteropTest extends HtsjdkTest {
     public static final String COMPRESSED_TOK_DIR = "tok3";
 
-    @DataProvider(name = "allNameTokenizationFiles")
-    public Object[][] getAllNameTokenizationCodecsForRoundTrip() throws IOException {
+    @DataProvider(name = "allNameTokenizationInteropTests")
+    public Object[][] getAllNameTokenizationInteropTests() throws IOException {
 
         // params:
         // compressed testfile path, uncompressed testfile path, NameTokenization encoder, NameTokenization decoder
         final List<Object[]> testCases = new ArrayList<>();
-        for (Path path : getInteropNameTokenizationCompressedFiles()) {
-                Object[] objects = new Object[]{
-                        path,
-                        getNameTokenizationUnCompressedFilePath(path),
-                        new NameTokenisationEncode(),
-                        new NameTokenisationDecode()
+        for (final Path preCompressedFilePath : getPreCompressedInteropNameTokTestFiles()) {
+                Object[] objects = new Object[] {
+                        preCompressedFilePath,
+                        getExpectedRawPathFromCompressedPath(preCompressedFilePath)
                 };
                 testCases.add(objects);
         }
         return testCases.toArray(new Object[][]{});
     }
 
+    //TODO: check in the interop streams and get rid of these getCorpus methods
     @Test(description = "Test if CRAM Interop Test Data is available")
     public void testGetHTSCodecsCorpus() {
         if (!CRAMInteropTestUtils.isInteropTestDataAvailable()) {
@@ -50,48 +49,50 @@ public class NameTokenizationInteropTest extends HtsjdkTest {
 
     @Test (
             dependsOnMethods = "testGetHTSCodecsCorpus",
-            dataProvider = "allNameTokenizationFiles",
+            dataProvider = "allNameTokenizationInteropTests",
             description = "Roundtrip using htsjdk NameTokenization Codec. Compare the output with the original file" )
-    public void testRangeRoundTrip(
+    public void testNameTokenizationRoundTrip(
             final Path precompressedFilePath,
-            final Path uncompressedFilePath,
-            final NameTokenisationEncode nameTokenisationEncode,
-            final NameTokenisationDecode nameTokenisationDecode) throws IOException {
-        try(final InputStream preCompressedInteropStream = Files.newInputStream(precompressedFilePath);
-            final InputStream unCompressedInteropStream = Files.newInputStream(uncompressedFilePath)){
-            final ByteBuffer preCompressedInteropBytes = ByteBuffer.wrap(IOUtils.toByteArray(preCompressedInteropStream));
-            final ByteBuffer unCompressedInteropBytes = ByteBuffer.wrap(IOUtils.toByteArray(unCompressedInteropStream));
-            ByteBuffer compressedHtsjdkBytes = nameTokenisationEncode.compress(unCompressedInteropBytes);
-            String decompressedHtsjdkString = nameTokenisationDecode.uncompress(compressedHtsjdkBytes);
-            ByteBuffer decompressedHtsjdkBytes = StandardCharsets.UTF_8.encode(decompressedHtsjdkString);
-            unCompressedInteropBytes.rewind();
-            Assert.assertEquals(decompressedHtsjdkBytes, unCompressedInteropBytes);
+            final Path expectedRawFilePath) throws IOException {
+
+        try (final InputStream expectedRawInteropStream = Files.newInputStream(expectedRawFilePath)) {
+            final ByteBuffer expectedRawInteropBytes = ByteBuffer.wrap(IOUtils.toByteArray(expectedRawInteropStream));
+
+            final NameTokenisationEncode nameTokenisationEncode = new NameTokenisationEncode();
+            final ByteBuffer compressedHtsjdkBytes = nameTokenisationEncode.compress(expectedRawInteropBytes);
+
+            final NameTokenisationDecode nameTokenisationDecode = new NameTokenisationDecode();
+            //TODO: get rid of the intermediate String returned by uncompress....
+            final String decompressedHtsjdkString = nameTokenisationDecode.uncompress(compressedHtsjdkBytes);
+
+            final ByteBuffer decompressedHtsjdkBytes = StandardCharsets.UTF_8.encode(decompressedHtsjdkString);
+            translateNameDelimiter(decompressedHtsjdkBytes);
+            expectedRawInteropBytes.rewind();
+            Assert.assertEquals(decompressedHtsjdkBytes, expectedRawInteropBytes);
         } catch (final NoSuchFileException ex){
             throw new SkipException("Skipping testRangeRoundTrip as either the input precompressed file " +
                     "or the uncompressed file is missing.", ex);
         }
     }
 
-
-
     @Test (
             dependsOnMethods = "testGetHTSCodecsCorpus",
-            dataProvider = "allNameTokenizationFiles",
+            dataProvider = "allNameTokenizationInteropTests",
             description = "Compress the original file using htsjdk NameTokenization Codec and compare it with the existing compressed file. " +
                     "Uncompress the existing compressed file using htsjdk NameTokenization Codec and compare it with the original file.")
-    public void testtNameTokenizationPreCompressed(
-            final Path compressedFilePath,
-            final Path uncompressedFilePath,
-            final NameTokenisationEncode unsusednameTokenisationEncode,
-            final NameTokenisationDecode nameTokenisationDecode) throws IOException {
-        try(final InputStream preCompressedInteropStream = Files.newInputStream(compressedFilePath);
-            final InputStream unCompressedInteropStream = Files.newInputStream(uncompressedFilePath)){
-            final ByteBuffer preCompressedInteropBytes = ByteBuffer.wrap(IOUtils.toByteArray(preCompressedInteropStream));
-            final ByteBuffer unCompressedInteropBytes = ByteBuffer.wrap(IOUtils.toByteArray(unCompressedInteropStream));
+    public void testNameTokenizationDecompress(
+            final Path precompressedFilePath,
+            final Path expectedUncompressedFilePath) throws IOException {
+        try (final InputStream preCompressedInteropStream = Files.newInputStream(precompressedFilePath);
+            final InputStream expectedUnCompressedStream = Files.newInputStream(expectedUncompressedFilePath)) {
 
+            final ByteBuffer preCompressedInteropBytes = ByteBuffer.wrap(IOUtils.toByteArray(preCompressedInteropStream));
+            final ByteBuffer unCompressedInteropBytes = ByteBuffer.wrap(IOUtils.toByteArray(expectedUnCompressedStream));
             // Use htsjdk to uncompress the precompressed file from htscodecs repo
+            final NameTokenisationDecode nameTokenisationDecode = new NameTokenisationDecode();
             final String uncompressedHtsjdkString = nameTokenisationDecode.uncompress(preCompressedInteropBytes);
-            ByteBuffer uncompressedHtsjdkBytes = StandardCharsets.UTF_8.encode(uncompressedHtsjdkString);
+            final ByteBuffer uncompressedHtsjdkBytes = StandardCharsets.UTF_8.encode(uncompressedHtsjdkString);
+            translateNameDelimiter(uncompressedHtsjdkBytes);
 
             // Compare the htsjdk uncompressed bytes with the original input file from htscodecs repo
             Assert.assertEquals(uncompressedHtsjdkBytes, unCompressedInteropBytes);
@@ -99,11 +100,10 @@ public class NameTokenizationInteropTest extends HtsjdkTest {
             throw new SkipException("Skipping testNameTokenizationPrecompressed as either input file " +
                     "or precompressed file is missing.", ex);
         }
-
     }
 
     // return a list of all NameTokenization encoded test data files in the htscodecs/tests/names/tok3 directory
-    private List<Path> getInteropNameTokenizationCompressedFiles() throws IOException {
+    private List<Path> getPreCompressedInteropNameTokTestFiles() throws IOException {
         final List<Path> paths = new ArrayList<>();
         Files.newDirectoryStream(
                 CRAMInteropTestUtils.getInteropTestDataLocation().resolve("names/"+COMPRESSED_TOK_DIR),
@@ -113,13 +113,13 @@ public class NameTokenizationInteropTest extends HtsjdkTest {
     }
 
     // Given a compressed test file path, return the corresponding uncompressed file path
-    public static final Path getNameTokenizationUnCompressedFilePath(final Path compressedInteropPath) {
-        String uncompressedFileName = getUncompressedFileName(compressedInteropPath.getFileName().toString());
+    private static final Path getExpectedRawPathFromCompressedPath(final Path compressedInteropPath) {
+        final String uncompressedFileName = getUncompressedFileName(compressedInteropPath.getFileName().toString());
         // Example compressedInteropPath: ../names/tok3/01.names.1 => unCompressedFilePath: ../names/01.names
         return compressedInteropPath.getParent().getParent().resolve(uncompressedFileName);
     }
 
-    public static final String getUncompressedFileName(final String compressedFileName) {
+    private static final String getUncompressedFileName(final String compressedFileName) {
         // Returns original filename from compressed file name
         int lastDotIndex = compressedFileName.lastIndexOf(".");
         if (lastDotIndex >= 0) {
@@ -131,4 +131,11 @@ public class NameTokenizationInteropTest extends HtsjdkTest {
         }
     }
 
+    private static void translateNameDelimiter(final ByteBuffer byteBuffer) {
+        for (int i = 0; i < byteBuffer.limit(); i++) {
+            if (byteBuffer.get(i) == ((byte) '\0')) {
+                byteBuffer.put(i, (byte) '\n');
+            }
+        }
+    }
 }
