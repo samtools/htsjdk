144f5f88115ee5cbd4b5f92dcb10139a35be30e4
Chris Norman <cnorman@broadinstitute.org>
2025-04-14 16:44:20 -0400

    Revert "Carrot tests - WIP."
    This reverts commit c8ba753f3e1c12772a41173ac8e77af4ff5abf59.

--- .carrot/run_test.sh
+++
@@ -1,1 +0,0 @@
-python3.10 -m carrot_cli test run --test_input test.json --eval_input eval.json 36d33423-15a5-4d8b-aa5d-cb04eb6f6017
--- .carrot/update_wdls.sh
+++
@@ -1,1 +0,0 @@
-python3.10 -m carrot_cli template update 03ff49e8-40ee-411e-b16f-f43b377cb6e9 --test_wdl carrot_test.wdl --eval_wdl carrot_eval.wdl
